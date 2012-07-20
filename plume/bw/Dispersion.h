2ed5a436c072d883a2401b7980b1d291f68f6863
Alex Geng <gengx064@d.umn.edu>
2012-07-20 16:02:35 -0500

    I changed somecode in bw/Dispersion.h, so it needs to update

---
+++ plume/bw/Dispersion.h
@@ -0,0 +1,35 @@
+#ifndef DISPERSION_H
+#define DISPERSION_H
+
+#include <list>
+#include <vector>
+#include <iostream>
+#include "Eulerian.h"
+#include "Random.h"
+#include <cutil_math.h>
+class dispersion{
+ public:
+  eulerian eul;
+  void createDisp(const eulerian&);
+  struct matrix{
+    double x;
+    double y;
+    double z;
+  };
+  
+ // std::vector<matrix> pos,prime;
+  std::vector<float3> pos,prime;
+  std::list<double> zIniPos;
+  std::list<double> wPrime;
+  double eps;
+  int numTimeStep;
+  std::vector<double> timeStepStamp,tStrt;
+  int parPerTimestep;
+  
+ private:
+  
+  double xSrc,ySrc,zSrc;
+  int numPar;
+	
+};
+#endif
