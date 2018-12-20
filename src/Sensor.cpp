e8951eef960928b4e262b7e8dd6b48767b829048
Behnam Bozorgmehr <behnambozorgmehr91@gmail.com>
2018-12-20 16:59:01 -0700

    Canopy type profile is been added as new velocity profile along with fixes for Barns scheme

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,20 +1,30 @@
 #include "Sensor.h"
 #include <math.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <chrono>
+#include <limits>
+
+using namespace std;
+
 
 
 
 void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int nz, double *u0, double *v0, double *w0, 
 							 int num_sites, int *site_blayer_flag, float *site_one_overL, float *site_xcoord, 
 							 float *site_ycoord, float *site_wind_dir, float *site_z0, float *site_z_ref, float *site_U_ref, 
-							 float *x, float *y, float *z)
+							 float *x, float *y, float *z, Canopy* canopy, float *site_canopy_H, float *site_atten_coeff)
 {
 
 	float psi, x_temp, u_star; 
-	float rc_sum, rc_value, xc, yc, rc, dn, lamda, s_gamma;
+	float rc_sum, rc_val, xc, yc, rc, dn, lamda, s_gamma;
 	float sum_wm, sum_wu, sum_wv;
-	int iwork, jwork,rc_val;
+	int iwork = 0, jwork = 0;
 	float dxx, dyy, u12, u34, v12, v34;
 	const float vk = 0.4;			/// Von Karman's constant
+	float canopy_d, u_H;
 
 	std::vector<std::vector<double>> u_prof(num_sites, std::vector<double>(nz,0.0));
 	std::vector<std::vector<double>> v_prof(num_sites, std::vector<double>(nz,0.0));
@@ -41,7 +51,7 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 
 		if (site_blayer_flag[i] == 1)
 		{
-			for (int k = 1; k < nz; k++)
+			for (int k = 1; k < nz-1; k++)
 			{
 				if (k == 1)
 				{
@@ -51,8 +61,8 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 					}
 					else 
 					{
-						x_temp = pow((1-15*site_z_ref[i]*site_one_overL[i]),0.25);
-						psi = -2*log(0.5*(1+x_temp))-log(0.5*(1+pow(x_temp,2.0)))+2*atan(x_temp)-0.5*M_PI;
+						x_temp = pow((1.0-15.0*site_z_ref[i]*site_one_overL[i]),0.25);
+						psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
 					}
 				
 					u_star = site_U_ref[i]*vk/(log((site_z_ref[i]+site_z0[i])/site_z0[i])+psi);
@@ -63,8 +73,8 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 				}
 				else 
 				{
-					x_temp = pow((1-15*z[k]*site_one_overL[i]),0.25);
-					psi = -2*log(0.5*(1+x_temp))-log(0.5*(1+pow(x_temp,2.0)))+2*atan(x_temp)-0.5*M_PI;
+					x_temp = pow((1.0-15.0*z[k]*site_one_overL[i]),0.25);
+					psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
 				}
 	
 				u_prof[i][k] = (cos(site_theta[i])*u_star/vk)*(log((z[k]+site_z0[i])/site_z0[i])+psi);
@@ -80,6 +90,77 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 				v_prof[i][k] = sin(site_theta[i])*site_U_ref[i]*pow((z[k]/site_z_ref[i]),site_z0[i]);
 			}
 		}
+
+		if (site_blayer_flag[i] == 3)
+		{
+			for (int k = 1; k< nz; k++)
+			{
+				if (k == 1)
+				{
+					if (site_z_ref[i]*site_one_overL[i] > 0)
+					{
+						psi = 4.7*site_z_ref[i]*site_one_overL[i];
+					}
+					else
+					{
+						x_temp = pow((1.0-15.0*site_z_ref[i]*site_one_overL[i]),0.25);
+						psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
+					}
+					u_star = site_U_ref[i]*vk/(log(site_z_ref[i]/site_z0[i])+psi);
+					canopy_d = canopy->bisection(u_star, site_z0[i], site_canopy_H[i], site_atten_coeff[i], vk, psi);
+					if (site_canopy_H[i]*site_one_overL[i] > 0)
+					{
+						psi = 4.7*(site_canopy_H[i]-canopy_d)*site_one_overL[i];
+					}
+					else
+					{
+						x_temp = pow((1.0-15.0*(site_canopy_H[i]-canopy_d)*site_one_overL[i]),0.25);
+						psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
+					}
+					u_H = (u_star/vk)*(log((site_canopy_H[i]-canopy_d)/site_z0[i])+psi);
+					if (site_z_ref[i] < site_canopy_H[i])
+					{
+						site_U_ref[i] /= u_H*exp(site_atten_coeff[i]*(site_z_ref[i]/site_canopy_H[i])-1.0);
+					}
+					else
+					{
+						if (site_z_ref[i]*site_one_overL[i] > 0)
+						{
+							psi = 4.7*(site_z_ref[i]-canopy_d)*site_one_overL[i];
+						}
+						else
+						{
+							x_temp = pow(1.0-15.0*(site_z_ref[i]-canopy_d)*site_one_overL[i],0.25);
+							psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
+						}
+						site_U_ref[i] /= ((u_star/vk)*(log((site_z_ref[i]-canopy_d)/site_z0[i])+psi));
+					}
+					u_star *= site_U_ref[i];
+					u_H *= site_U_ref[i];
+				}
+				
+				if (z[k] < site_canopy_H[i])
+				{
+					u_prof[i][k] = cos(site_theta[i]) * u_H*exp(site_atten_coeff[i]*((z[k]/site_canopy_H[i]) -1.0));
+					v_prof[i][k] = sin(site_theta[i]) * u_H*exp(site_atten_coeff[i]*((z[k]/site_canopy_H[i]) -1.0));
+				}
+				if (z[k] > site_canopy_H[i])
+				{
+					if (z[k]*site_one_overL[i] > 0)
+					{
+						psi = 4.7*(z[k]-canopy_d)*site_one_overL[i];
+					}
+					else
+					{
+						x_temp = pow(1.0-15.0*(z[k]-canopy_d)*site_one_overL[i],0.25);
+						psi = -2.0*log(0.5*(1.0+x_temp))-log(0.5*(1.0+pow(x_temp,2.0)))+2.0*atan(x_temp)-0.5*M_PI;
+					}
+					u_prof[i][k] = (cos(site_theta[i])*u_star/vk)*(log((z[k]-canopy_d)/site_z0[i])+psi);
+					v_prof[i][k] = (sin(site_theta[i])*u_star/vk)*(log((z[k]-canopy_d)/site_z0[i])+psi);	
+				}
+			}
+		}
+
 	}
 
 
@@ -115,21 +196,21 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 					rc_val = rc;
 				}
 			}
-			rc_sum += rc_val;
+			rc_sum = rc_sum+rc_val;
 		}
 		dn = rc_sum/num_sites;
 		lamda = 5.052*pow((2*dn/M_PI),2.0);
 		s_gamma = 0.2;
-		for (int j=0; j<ny; j++)
+		for (int j=0; j<ny-1; j++)
 		{
-			for (int i=0; i<nx; i++)
+			for (int i=0; i<nx-1; i++)
 			{
 				sum_wm = 0.0;
 				for (int ii=0; ii<num_sites; ii++)
 				{
-					wm[ii][i][j] = exp(-1/lamda*pow(site_xcoord[ii]-x[i]-dx,2.0)-1/lamda*pow(site_ycoord[ii]-y[j]-dy,2.0));
-					wms[ii][i][j] = exp(-1/(s_gamma*lamda)*pow(site_xcoord[ii]-x[i]-dx,2.0)-1/(s_gamma*lamda)*
-										pow(site_ycoord[ii]-y[j]-dy,2.0));
+					wm[ii][i][j] = exp((-1/lamda)*pow(site_xcoord[ii]-x[i],2.0)-(1/lamda)*pow(site_ycoord[ii]-y[j],2.0));
+					wms[ii][i][j] = exp((-1/(s_gamma*lamda))*pow(site_xcoord[ii]-x[i],2.0)-(1/(s_gamma*lamda))*
+										pow(site_ycoord[ii]-y[j],2.0));
 					sum_wm += wm[ii][i][j];
 				}
 				if (sum_wm == 0)
@@ -142,11 +223,11 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 			}
 		}
 	
-		for (int k=1; k<nz; k++)
+		for (int k=1; k<nz-1; k++)
 		{
-			for (int j=0; j<ny; j++)
+			for (int j=0; j<ny-1; j++)
 			{
-				for (int i=0; i<nx; i++)
+				for (int i=0; i<nx-1; i++)
 				{
 					sum_wu = 0.0;
 					sum_wv = 0.0;
@@ -166,16 +247,16 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 	
 			for (int ii=0; ii<num_sites; ii++)
 			{
-				if(site_xcoord[ii]>0 && site_xcoord[ii]<(nx-1)*dx && site_ycoord[ii]>0 && site_ycoord[ii]>(ny-1)*dy)
+				if(site_xcoord[ii]>0 && site_xcoord[ii]<(nx-1)*dx && site_ycoord[ii]>0 && site_ycoord[ii]<(ny-1)*dy)
 				{
-					for (int j=0; j<ny; j++)
+					for (int j=0; j<ny-1; j++)
 					{
 						if (y[j]<site_ycoord[ii])
 						{
 							jwork = j;
 						}
 					}
-					for (int i=0; i<nx; i++)
+					for (int i=0; i<nx-1; i++)
 					{
 						if (x[i]<site_xcoord[ii])
 						{
@@ -185,13 +266,13 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 					dxx = site_xcoord[ii]-x[iwork];
 					dyy = site_ycoord[ii]-y[jwork];
 					int index_work = iwork+jwork*nx+k*nx*ny;
-					u12 = (1-dxx/dx)*u0[index_work+nx]+(dxx/dx)*u0[index_work+1+nx];
-					u34 = (1-dxx/dx)*u0[index_work]+(dxx/dx)*u0[index_work+1];
-					u0_int[ii] = (dyy/dy)*u12+(1-dyy/dy)*u34;
+					u12 = (1-(dxx/dx))*u0[index_work+nx]+(dxx/dx)*u0[index_work+1+nx];
+					u34 = (1-(dxx/dx))*u0[index_work]+(dxx/dx)*u0[index_work+1];
+					u0_int[ii] = (dyy/dy)*u12+(1-(dyy/dy))*u34;
 		
-					v12 = (1-dxx/dx)*v0[index_work+nx]+(dxx/dx)*v0[index_work+1+nx];
-					v34 = (1-dxx/dx)*v0[index_work]+(dxx/dx)*v0[index_work+1];
-					v0_int[ii] = (dyy/dy)*v12+(1-dyy/dy)*v34;
+					v12 = (1-(dxx/dx))*v0[index_work+nx]+(dxx/dx)*v0[index_work+1+nx];
+					v34 = (1-(dxx/dx))*v0[index_work]+(dxx/dx)*v0[index_work+1];
+					v0_int[ii] = (dyy/dy)*v12+(1-(dyy/dy))*v34;
 				}
 				else
 				{
@@ -200,17 +281,17 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 				}
 			}
 	
-			for (int j=0; j<ny; j++)
+			for (int j=0; j<ny-1; j++)
 			{
-				for (int i=0; i<nx; i++)
+				for (int i=0; i<nx-1; i++)
 				{
 					sum_wu = 0.0;
 					sum_wv = 0.0;
 					sum_wm = 0.0;
 					for (int ii=0; ii<num_sites; ii++)
 					{
-						sum_wu += wm[ii][i][j]*u_prof[ii][k];
-						sum_wv += wm[ii][i][j]*v_prof[ii][k];	
+						sum_wu += wm[ii][i][j]*(u_prof[ii][k]-u0_int[ii]);
+						sum_wv += wm[ii][i][j]*(v_prof[ii][k]-v0_int[ii]);	
 						sum_wm += wm[ii][i][j];
 					}
 					if (sum_wm != 0)
@@ -223,5 +304,6 @@ void Sensor::inputWindProfile(float dx, float dy, float dz, int nx, int ny, int 
 			}
 		}
 	}
+
 }
 
