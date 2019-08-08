f15b66a0d8c6b40317cffaee5d92e36e9f843008
Behnam Bozorgmehr <behnambozorgmehr91@gmail.com>
2019-08-08 14:31:37 -0600

    Changed some of the icellflag values.

--- src/URBInputData.h
+++ src/URBInputData.h
@@ -24,19 +24,19 @@ public:
 
     URBInputData()
     {
-	fileOptions = 0;
-	metParams = 0;
-	buildings = 0;
-	canopies = 0;
+	      fileOptions = 0;
+	      metParams = 0;
+	      buildings = 0;
+	      canopies = 0;
     }
 
     virtual void parseValues()
     {
-	parseElement<SimulationParameters>(true, simParams, "simulationParameters");
-	parseElement<FileOptions>(false, fileOptions, "fileOptions");
-	parseElement<MetParams>(false, metParams, "metParams");
-	parseElement<Buildings>(false, buildings, "buildings");
-	parseElement<Canopies>(false, canopies, "canopies");
+	     parseElement<SimulationParameters>(true, simParams, "simulationParameters");
+	     parseElement<FileOptions>(false, fileOptions, "fileOptions");
+	     parseElement<MetParams>(false, metParams, "metParams");
+	     parseElement<Buildings>(false, buildings, "buildings");
+	     parseElement<Canopies>(false, canopies, "canopies");
     }
 
     /**
@@ -46,7 +46,7 @@ public:
      * XML file used to represent projects in the QUIC system.
      */
     void parseTree(pt::ptree t)
-    { 
+    {
         setTree(t);
         setParents("root");
         parseValues();
