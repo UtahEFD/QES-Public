ab4f9731961bea9cf82b9de5e0c02c51869b73e0
Fabien Margairaz <fabien.margairaz@utah.edu>
2019-09-30 16:01:08 +0200

    generic output with overloaded function for int/flt/dbl

--- src/URBOutput_Generic.cpp
+++ src/URBOutput_Generic.cpp
@@ -5,35 +5,149 @@ URBOutput_Generic::URBOutput_Generic(std::string output_file)
 {
 };
 
+/*
+  Attribute are create based and the type of the data
+  -> attribute are store in map_att_*
+  -> all possible attribute available for the derived class should be 
+  created by its constructor. 
+  -> attribute are pushed back to output_* based on what is selected 
+  by output_fields 
+  -> the methods allows to by type generic (as long as the data is 
+  either int,float or double
+ */
+
+//----------------------------------------
+// create attribute scalar
+// -> float
+void URBOutput_Generic::createAttSacalar(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 int* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttScalarInt att = {&data,name,long_name,units,dims};
+  map_att_scalar_flt.emplace(name,att);
+  
+}
+// -> float
+void URBOutput_Generic::createAttSacalar(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 float* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttScalarFlt att = {&data,name,long_name,units,dims};
+  map_att_scalar_flt.emplace(name,att);
+  
+}
+// -> double
+void URBOutput_Generic::createAttSacalar(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 double* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttScalarDbl att = {&data,name,long_name,units,dims};
+  map_att_scalar_dbl.emplace(name,att);
+}
+
+//----------------------------------------
+// create attribute Vector
+// -> int
+void URBOutput_Generic::createAttVector(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 int* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttVectorInt att = {&data,name,long_name,units,dims};
+  map_att_vector_int.emplace(name,att);
+  
+}
+// -> float
+void URBOutput_Generic::createAttVector(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 float* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttVectorFlt att = {&data,name,long_name,units,dims};
+  map_att_vector_flt.emplace(name,att);
+  
+}
+// -> double
+void URBOutput_Generic::createAttVector(std::string name,
+					 std::string long_name,
+					 std::string units,
+					 std::vector<NcDim> dims,
+					 double* data)
+{
+  // FM -> here I do not know what is the best way to add the ref to data.
+  AttVectorDbl att = {&data,name,long_name,units,dims};
+  map_att_vector_dbl.emplace(name,att);
+}
+
+//----------------------------------------
 void URBOutput_Generic::addOutputFields()
 {
-  // create list of fields to save
+  
+  // create list of fields to save base on output_fields
   for (size_t i=0; i<output_fields.size(); i++) {
     std::string key = output_fields[i];
-    if (map_att_scalar_dbl.count(key)) {
-      output_scalar_dbl.push_back(map_att_scalar_dbl[key]);
+    
+    if (map_att_scalar_int.count(key)) {
+      // scalar int
+      output_scalar_int.push_back(map_att_scalar_int[key]);
     } else if (map_att_vector_dbl.count(key)) {
-      output_vector_dbl.push_back(map_att_vector_dbl[key]);
+      // scalar flt
+      output_vector_flt.push_back(map_att_vector_dbl[key]);
+    } else if (map_att_vector_dbl.count(key)) {
+      // scalar dbl
+      output_vector_dbl.push_back(map_att_vector_dbl[key]);  
     } else if(map_att_vector_int.count(key)) {
+      // vector int
       output_vector_int.push_back(map_att_vector_int[key]);
+    } else if(map_att_vector_flt.count(key)) {
+      // vector flt
+      output_vector_flt.push_back(map_att_vector_flt[key]);    
+    } else if (map_att_vector_dbl.count(key)) {
+      // vector dbl
+      output_vector_dbl.push_back(map_att_vector_dbl[key]);
     }
   }
 
-  // add vector double fields
+  // add scalar fields
+  // -> int
+  for ( AttScalarInt att : output_scalar_int ) {
+    addField(att.name, att.units, att.long_name, att.dimensions, ncDouble);
+  }
+  // -> float
+  for ( AttScalarFlt att : output_scalar_flt ) {
+    addField(att.name, att.units, att.long_name, att.dimensions, ncDouble);
+  }
+  // -> double
   for ( AttScalarDbl att : output_scalar_dbl ) {
     addField(att.name, att.units, att.long_name, att.dimensions, ncDouble);
   }
 
-   // add vector double fields
-  for ( AttVectorDbl att : output_vector_dbl ) {
-    addField(att.name, att.units, att.long_name, att.dimensions, ncDouble);
-  }
-
-  // add vector int fields
+  // add vector fields
+  // -> int
   for ( AttVectorInt att : output_vector_int ) {
     addField(att.name, att.units, att.long_name, att.dimensions, ncInt);
   }
-
+  // -> int
+  for ( AttVectorFlt att : output_vector_flt ) {
+    addField(att.name, att.units, att.long_name, att.dimensions, ncInt);
+  }x
+  // -> double
+  for ( AttVectorDbl att : output_vector_dbl ) {
+    addField(att.name, att.units, att.long_name, att.dimensions, ncDouble);
+  }
 };
 
 void URBOutput_Generic::saveOutputFields()
@@ -49,7 +163,8 @@ void URBOutput_Generic::saveOutputFields()
      if 2D array -> can get nx,ny
      if only 1D -> ambuguous desciption, cannot get dimensions
   */
-  
+
+  // FM need to change this as it is not generic for int/float/double
   std::vector<int> dims_vec_dbl;
   for (auto i=0u; i<output_vector_dbl.size(); i++) {
     dims_vec_dbl.push_back(output_vector_dbl[i].dimensions.size());
@@ -95,31 +210,75 @@ void URBOutput_Generic::saveOutputFields()
   vector_size_2d  = {static_cast<unsigned long>(ny_out),
 		     static_cast<unsigned long>(nx_out)};
   
-  // loop through 1D fields to save
-  for (unsigned int i=0; i<output_scalar_dbl.size(); i++) {
-    saveField1D(output_scalar_dbl[i].name, scalar_index, output_scalar_dbl[i].data);
+  // loop through scalar fields to save
+  // -> int
+  for (unsigned int i=0; i<output_scalar_int.size(); i++) {
+    saveField1D(output_scalar_int[i].name, scalar_index,
+		output_scalar_int[i].data);
   }
-    
-  // loop through double fields to save
+  // -> float
+  for (unsigned int i=0; i<output_scalar_flt.size(); i++) {
+    saveField1D(output_scalar_flt[i].name, scalar_index,
+		output_scalar_flt[i].data);
+  }
+  // -> double
+  for (unsigned int i=0; i<output_scalar_dbl.size(); i++) {
+    saveField1D(output_scalar_dbl[i].name, scalar_index,
+		output_scalar_dbl[i].data);
+  }
+
+  
+  // loop through vectore fields to save
+  // -> int
+  for (unsigned int i=0; i<output_vector_int.size(); i++) {
+    // 1D (x or z or y)
+    if (output_vector_int[i].dimensions.size()==1) {
+      saveField2D(output_vector_int[i].name, *output_vector_int[i].data);
+    } 
+    // 2D (terrain)
+    else if (output_vector_int[i].dimensions.size()==2) {
+      saveField2D(output_vector_int[i].name, vector_index_2d,
+		  vector_size_2d, *output_vector_int[i].data);
+    }
+    // 4D (u,v,w)
+    else if (output_vector_int[i].dimensions.size()==4) {
+      saveField2D(output_vector_int[i].name, vector_index,
+		  vector_size, *output_vector_int[i].data);
+    }
+  }
+  // -> float
+  for (unsigned int i=0; i<output_vector_flt.size(); i++) { 
+    // 1D (x or z or y)
+    if (output_vector_flt[i].dimensions.size()==1) {
+      saveField2D(output_vector_flt[i].name, *output_vector_flt[i].data);
+    } 
+    // 2D (terrain)
+    else if (output_vector_flt[i].dimensions.size()==2) {
+      saveField2D(output_vector_flt[i].name, vector_index_2d,
+		  vector_size_2d, *output_vector_flt[i].data);
+    }
+    // 4D (u,v,w)
+    else if (output_vector_flt[i].dimensions.size()==4) {
+      saveField2D(output_vector_flt[i].name, vector_index,
+		  vector_size, *output_vector_flt[i].data);
+    }
+  }
+  // -> double
   for (unsigned int i=0; i<output_vector_dbl.size(); i++) {
-      
-    // 1D (x,z,y)
+    // 1D (x or z or y)
     if (output_vector_dbl[i].dimensions.size()==1) {
       saveField2D(output_vector_dbl[i].name, *output_vector_dbl[i].data);
     } 
     // 2D (terrain)
     else if (output_vector_dbl[i].dimensions.size()==2) {
-      saveField2D(output_vector_dbl[i].name, vector_index_2d, vector_size_2d, *output_vector_dbl[i].data);
+      saveField2D(output_vector_dbl[i].name, vector_index_2d,
+		  vector_size_2d, *output_vector_dbl[i].data);
     }
     // 4D (u,v,w)
-    else if (output_vector_dbl[i].dimensions.size()==4) {
-      saveField2D(output_vector_dbl[i].name, vector_index, vector_size, *output_vector_dbl[i].data);
+    else if (output_vector_dbl[i].dimensions.sizexs()==4) {
+      saveField2D(output_vector_dbl[i].name, vector_index,
+		  vector_size, *output_vector_dbl[i].data);
     }
-  }
-    
-  // loop through 2D int fields to save
-  for (unsigned int i=0; i<output_vector_int.size(); i++) {
-    saveField2D(output_vector_int[i].name, vector_index, vector_size, *output_vector_int[i].data);
   }
 
 };
--- src/URBOutput_Generic.h
+++ src/URBOutput_Generic.h
@@ -16,6 +16,29 @@
 using namespace netCDF;
 using namespace netCDF::exceptions;
 
+struct AttVectorInt {
+  std::vector<int>* data;
+  std::string name;
+  std::string long_name;
+  std::string units;
+  std::vector<NcDim> dimensions;
+};
+
+struct AttScalarFlt {
+  float* data;
+  std::string name;
+  std::string long_name;
+  std::string units;
+  std::vector<NcDim> dimensions;
+};
+struct AttVectorFlt {
+  std::vector<float>* data;
+  std::string name;
+  std::string long_name;
+  std::string units;
+  std::vector<NcDim> dimensions;
+};
+
 struct AttScalarDbl {
   double* data;
   std::string name;
@@ -31,29 +54,6 @@ struct AttVectorDbl {
   std::vector<NcDim> dimensions;
 };
 
-struct AttScalarFlt {
-  double* data;
-  std::string name;
-  std::string long_name;
-  std::string units;
-  std::vector<NcDim> dimensions;
-};
-struct AttVectorFlt {
-  std::vector<double>* data;
-  std::string name;
-  std::string long_name;
-  std::string units;
-  std::vector<NcDim> dimensions;
-};
-
-struct AttVectorInt {
-  std::vector<int>* data;
-  std::string name;
-  std::string long_name;
-  std::string units;
-  std::vector<NcDim> dimensions;
-};
-
 class URBOutput_Generic : public NetCDFOutput 
 {
  public:
@@ -62,8 +62,26 @@ class URBOutput_Generic : public NetCDFOutput
   URBOutput_Generic(std::string);
   virtual ~URBOutput_Generic()
     {}
+    
+  // create attribute scalar based on type of data
+  void createAttScalar(std::string,std::string,std::string,
+		       std::vector<NcDim>,int*);
+  void createAttScalar(std::string,std::string,std::string,
+		       std::vector<NcDim>,float*);
+  void createAttScalar(std::string,std::string,std::string,
+		       std::vector<NcDim>,double*);
   
+  // create attribute vector based on type of data
+  void createAttVector(std::string,std::string,std::string,
+		       std::vector<NcDim>,int*);
+  void createAttVector(std::string,std::string,std::string,
+		       std::vector<NcDim>,float*);
+  void createAttVector(std::string,std::string,std::string,
+		       std::vector<NcDim>,double*);
+
+  // add fields based on output_fields
   void addOutputFields();
+  // save fields 
   void saveOutputFields();
 
  protected:
@@ -88,15 +106,19 @@ class URBOutput_Generic : public NetCDFOutput
 
   std::vector<std::string> output_fields;
 
+  std::map<std::string,AttScalarInt> map_att_scalar_int;
+  std::map<std::string,AttScalarFlt> map_att_scalar_flt;
   std::map<std::string,AttScalarDbl> map_att_scalar_dbl;
+  std::map<std::string,AttVectorInt> map_att_vector_int;
+  std::map<std::string,AttVectorFlt> map_att_vector_flt;
   std::map<std::string,AttVectorDbl> map_att_vector_dbl;
 
-  std::map<std::string,AttVectorInt> map_att_vector_int;
-
+  std::vector<AttScalarInt> output_scalar_int;
+  std::vector<AttScalarFlt> output_scalar_flt;
   std::vector<AttScalarDbl> output_scalar_dbl;
-  std::vector<AttVectorDbl> output_vector_dbl;
-
   std::vector<AttVectorInt> output_vector_int;
+  std::vector<AttVectorFlt> output_vector_flt;
+  std::vector<AttVectorDbl> output_vector_dbl;
 
 };
 
