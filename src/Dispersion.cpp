f03415b5806e750c9bec55a00015efd9863c12a1
latwood <pianotocador@gmail.com>
2019-11-08 22:30:50 -0800

    cleaned up the Dispersion class so it is much easier to read and understand. It also has the potential for multiple sources now, though the source initialization factory method won't work for multiple sources because the source type as a string is not stored in the input classes. But the code at least works right now still, even if implementing more sources would require moving the Dispersion::addSource() functions to the input source classes, getting rid of the initialization factory idea

--- src/Dispersion.cpp
+++ src/Dispersion.cpp
@@ -8,7 +8,9 @@
 #include <fstream>
 #define EPSILON 0.00001   
 
-Dispersion::Dispersion(Urb* urb, Turb* turb, PlumeInputData* PID, Eulerian* eul) {
+Dispersion::Dispersion(Urb* urb, Turb* turb, PlumeInputData* PID, Eulerian* eul)
+{
+
     std::cout<<"[Dispersion] \t Setting up sources "<<std::endl;
     
     // make local copies
@@ -18,10 +20,27 @@ Dispersion::Dispersion(Urb* urb, Turb* turb, PlumeInputData* PID, Eulerian* eul)
     dy     = urb->grid.dy;
     dz     = urb->grid.dz;
     dt     = PID->simParams->timeStep;
-    dur    = PID->simParams->runTime;
+    simDur    = PID->simParams->runTime;
+
+    // set up time details
+    numTimeStep = std::ceil(simDur/dt);
+    timeStepStamp.resize(numTimeStep);
+    for(int i = 0; i < numTimeStep; ++i)
+    {
+        timeStepStamp.at(i) = i*dt + dt;
+    }
 
 
-    addSource(PID->sources, turb, eul);
+    // this function goes through each source and adds to the particle list the initial positions and release times
+    // this function is an initialization factory calling an add source for each type of source
+    // eventually this all needs moved to be a single virtual function inside of sourceKind that is overloaded by each source
+    // then the if statements in the initialization factory function will go away
+    // I think the new virtual function would need to be called something else though, like generatePointInfo()
+    addSources(PID->sources);
+
+    // now that all the sources have added to the particle list, it's time to setup the initial values for each particle using their initial positions
+    setParticleVals(turb, eul);
+
 
     // set the isRogueCount to zero
     isRogueCount = 0.0;
@@ -33,8 +52,10 @@ Dispersion::Dispersion(Urb* urb, Turb* turb, PlumeInputData* PID, Eulerian* eul)
     /*
       Checking if the starting time for the last particle is equal to the duration of
       the simulation (for continous release ONLY)
+      would need to modify this to check all particles, probably better to just do the check at the end of each source, 
+      so it is known which source causes the problem
     */
-    if (fabs(tStrt.back()-dur)>EPSILON) {
+    if (fabs(tStrt.back()-simDur)>EPSILON) {
         std::cerr<<" Error, in start time of the particles"<<std::endl;
         exit(1);
     }
@@ -42,22 +63,96 @@ Dispersion::Dispersion(Urb* urb, Turb* turb, PlumeInputData* PID, Eulerian* eul)
 }
 
 
-void Dispersion::addSource(Sources* source, Turb* turb, Eulerian* eul)
+void Dispersion::addSources(Sources* sources)
 {
-    numPar = source->numParticles;
-    srcX   = ((SourcePoint*)source->sources.at(0))->posX;
-    srcY   = ((SourcePoint*)source->sources.at(0))->posY;
-    srcZ   = ((SourcePoint*)source->sources.at(0))->posZ;
+    // first get the number of sources from the sources variable
+    int numSources = sources->numSources;
+
+    // also grab the number of particles, since that is where this variable is stored for now. I want to eventually move this to the individual sources
+    // then numPar will represent a cumulative number from each source
+    numPar = sources->numParticles;
+
     
-    // set up time details
-    numTimeStep = std::ceil(dur/dt);
-    timeStepStamp.resize(numTimeStep);
-    for(int i=0;i<numTimeStep;++i){ 
-        timeStepStamp.at(i)=i*dt+dt;
+    for(int i = 0; i < numSources; i++)
+    {
+        // right now I use if statements because this is an initialization factory, but at some time, I hope to move these function calls
+        // into the source classes themselves, as a single virtual function that is overloaded by each source
+        //   oh crap, the name of the source type isn't even stored, so this won't even work
+        // so I'm not going to do if statements for now, just going to call the function that does the source type
+        // even worse, without using an if statement I've created a new pointer each iteration that I technically want to get rid of
+        // not sure if this would cause memory leak. For now, I'll hope it gets deleted at each iteration
+        // but hopefully this doesn't mean I'm accidentally deleting the original pointer to the source?
+        // I think it should at least run still as is
+        SourcePoint* currentSource = (SourcePoint*)sources->sources.at(i);
+
+        addSource(currentSource);
+
     }
+
+}
+
+
+void Dispersion::addSource(SourcePoint* source)
+{
+    // for now this is an okay spot, when adding multiple sources, this variable needs updated/added to, or at least
+    // needs to be a temporary one for unpacking these values into the full list of values
+    // oh crap, I want an individual value for this for each source rather than a single value for all sources
+    // but it isn't setup this way in the input files yet, so I can't do that. But right now I'm forcing a source type
+    // so the value isn't inside that storage yet either
+    // so going to comment this out for now
+    //numPar = source->numParticles;
     
-    // set up source information
+    // these are the source point x, y, and z values from the input source information
+    double srcX = source->posX;
+    double srcY = source->posY;
+    double srcZ = source->posZ;
+    
+    
+    // set up source information sizes
     pos.resize(numPar);
+    tStrt.resize(numPar);
+    
+
+    // now set source positions
+    for(int i = 0; i < numPar; i++)
+    {
+
+        pos.at(i).e11 = srcX;   // set the source positions for each particle
+        pos.at(i).e21 = srcY;
+        pos.at(i).e31 = srcZ;
+
+    }
+
+    // setup the number of particles per timestep
+    // this should eventually be adjusting this value, not adding to it
+    parPerTimestep = numPar*dt/simDur;
+    std::cout << "[Dispersion] \t Emitting " << parPerTimestep << " particles per Time Step" << std::endl;
+    
+    int parRel = 0;
+    double startTime = dt;
+    
+    // now set source release times
+    for(int i = 0; i < numPar; i++)
+    {
+      /*when number of particles to be released in a particluar time step reaches total 
+        number of particles to be released in that time step, then increase the start time 
+        by timestep and set parRel=0*/
+        if( parRel == parPerTimestep )
+        {
+            startTime = startTime + dt;
+            parRel = 0;
+        }
+        tStrt.at(i) = startTime;
+        ++parRel;
+    }
+
+
+}
+
+void Dispersion::setParticleVals(Turb* turb, Eulerian* eul)
+{
+
+    // now set source value size
     prime.resize(numPar);
 
     // set up the initial conditions for the old values too
@@ -68,18 +163,16 @@ void Dispersion::addSource(Sources* source, Turb* turb, Eulerian* eul)
     isActive.resize(numPar);
 
     
-    for(int i=0;i<numPar;i++)
+    // at this time, should be a list of each and every particle that exists
+    // might need to vary this to allow for adding sources after the initial constructor
+    for(int i = 0; i < numPar; i++)
     {
-
-        pos.at(i).e11=srcX;   // set the source positions for each particle
-        pos.at(i).e21=srcY;
-        pos.at(i).e31=srcZ;
 
         // this replaces the old indexing trick, set the indexing variables for the interp3D for each particle,
         // then get interpolated values from the Eulerian grid to the particle Lagrangian values for multiple datatypes
         eul->setInterp3Dindexing(pos.at(i));
     
-        double rann=random::norRan();   // almost didn't see it, but it does use different random numbers for each direction
+        double rann = random::norRan();   // almost didn't see it, but it does use different random numbers for each direction
 
         // get the sigma values from the Eulerian grid for the particle value
         diagonal current_sig = eul->interp3D(turb->sig,"sigma2");
@@ -118,25 +211,6 @@ void Dispersion::addSource(Sources* source, Turb* turb, Eulerian* eul)
         
     }
 
-    tStrt.resize(numPar);
-    
-    parPerTimestep = numPar*dt/dur; 
-    std::cout<<"[Dispersion] \t Emitting "<<parPerTimestep<< " particles per Time Step"<<std::endl;
-    
-    int parRel = 0;
-    double startTime = dt;
-    
-    for(int i=0;i<numPar;i++){
-      /*when number of particles to be released in a particluar time step reaches total 
-        number of particles to be released in that time step, then increase the start time 
-        by timestep and set parRel=0*/
-        if(parRel==parPerTimestep) {
-            startTime=startTime+dt;
-            parRel=0;
-        }
-        tStrt.at(i)=startTime;
-        ++parRel;
-    }
 
 }
 
--- src/Dispersion.h
+++ src/Dispersion.h
@@ -21,54 +21,68 @@ class Dispersion {
     
     public:
         
-        Dispersion(Urb*,Turb*,PlumeInputData*,Eulerian*); // starts by making copies of nx,ny, dx,dy,dz, numParticles, timestep,runTime, and source->posX,posY,posZ
-                                                // then calculates the number of timesteps numTimeStep from runTime and timestep.
-                                                // then calculates the list of times timeStepStamp
-                                                // then sets the pos and prime values for each particle, which are the starting positions and the starting values for each particle
-                                                // then calculate the number of particles to release at each time, so the total number to release is split up for each time
-                                                // yeah we need to change how this is done cause I want to release once at the start. Need to set time start for release, and time end for release
-                                                // then finally set the release times for each and every particle, since they are released constantly over time
-
+        Dispersion(Urb*,Turb*,PlumeInputData*,Eulerian*); // starts by making copies of nx,ny, dx,dy,dz, timestep, and runTime
+                                                // then calculates the number of timesteps called numTimeStep from runTime and timestep.
+                                                // then calculates the list of times called timeStepStamp
+                                                // next, it calls addSource() to go through each source and set the initial positions and release times for each particle
+                                                // then it calls setParticleVals() to set the initial values for each particle using the list of positions and the urb and turb data
+                                                // lastly, some values that are not unique to each particle are set such as the number of rogue particles, called isRogueCount
+                                                //  and the velocity threshold used to know if particles are rogue
+                                                // finally, and this probably needs adjusted to look through each particle instead of just doing this for the last particle, 
+                                                //  the release times are checked to make sure they aren't past the simulation time
+                                                
         // looks like this is just defining a list of information for Plume to use to know where and when to release different particles.
         // so defining where and when every single particle is released over the entire simulation.
-        // looks like there needs to be a lot of work here. I thought the source had a radius as well as a point input, but here we only use a single point location
-        // a lot of work needs done to get the source stuff up to par. Right now can only release at a point, and it is ugly to figure out how to use this
-        // current method will NOT work for Brian's test cases, as they release all over the entire domain, only at the very start of the simulation
+        // this also holds the full list of particle information, so the dispersion class could probably be renamed to Lagrangian since it is the Lagrangian grid of values
         
-        // the way this information is used in Plume right now is as follows:
-        // 
-
+        
+        int numTimeStep;            // this is the number of timesteps of the simulation
+        std::vector<double> timeStepStamp;  // this is the list of times for the simulation
         
         double isRogueCount;        // just a total number of rogue particles per time iteration
         double vel_threshold;       // the velocity fluctuation threshold velocity used to determine if particles are rogue or no
 
-        int numTimeStep;            // this is the number of timesteps of the simulation
+        
+        // at some point in time, this would probably be better as a vector, so the number can vary for each timestep
+        // then, this variable would act like a counter to the full list of particles. Hm, this gets more complex than that when recycling particles
+        int parPerTimestep;             // this is the number of particles to release at each timestep, used to calculate the release time for each particle in Dispersion, and also to update the particle loop counter in Plume.
         
 
-        std::vector<double> timeStepStamp;  // this is the list of times for the simulation
-        std::vector<vec3> pos, prime;     // pos is the list of particle source positions, prime is the particle source values. Actually, the prime is the velFluct value for a given iteration!
+        // the sources can set these values, then the other values are set using urb and turb info using these values
+        std::vector<vec3> pos;          // pos is the list of particle source positions
+        std::vector<double> tStrt;        // this is the time of release for each set of particles
+        
+        // once positions are known, can set these values
+        std::vector<vec3> prime;     // prime is the velFluct value for a given iteration. Starts out as the initial value until a particle is "released" into the domain
         std::vector<vec3> prime_old;     // this is the velocity fluctuations from the last iteration. They start out the same as the current values initially
         std::vector<matrix6> tau_old;       // this is the stress tensor from the last iteration. Starts out as the values at the initial position, the values for the initial iteration
         std::vector<vec3> delta_prime;    // this is the difference between the current and last iteration of the velocity fluctuations
         std::vector<bool> isRogue;         // this is false until it becomes true. Should not go true. It is whether a particle has gone rogue or not
         std::vector<bool> isActive;         // this is true until it becomes false. If a particle leaves the domain or runs out of mass, this becomes false. Later we will add a method to start more particles when this has become false
-        std::vector<double> tStrt;        // this is the time of release for each set of particles
-        int parPerTimestep;             // this is the number of particles to release at each timestep, used to calculate the release time for each particle
         
 
-        void addSource(Sources* source, Turb* turb, Eulerian* eul);       // this function is for adding a generic source of type . . .
+        // going to have a ton of source functions, this first one will be the source factory operator which calls all the other add source functions
+        // depending on the type of source. It should be noted that the end goal will be to get rid of the initialization factory, probably by
+        // putting all the add source functions into the sources themselves, probably as a function that just finds the initial particle positions and release times
+        // probably all the other variables can be set depending on the source positions, maybe, we shall have to see
+        void addSources(Sources* sources);       // this function is for adding a generic source of type . . .
+        void addSource(SourcePoint* source);   // this is the Continuous SourcePoint type
+
+        void setParticleVals(Turb* turb, Eulerian* eul);
         
 
     private:
         
-        int nx,ny;              // these are copies of the Urb grid nx and ny values. Not sure why nz isn't included
-        double dx,dy,dz;        // these are copies of the Urb grid dx,dy,dz values.
+        // just realized, what if urb and turb have different grids? For now assume they are the same grid
+        int nx,ny;              // these are copies of the Urb grid nx and ny values. Not sure why nz isn't included.    I don't think these are even used
+        double dx,dy,dz;        // these are copies of the Urb grid dx,dy,dz values.            I don't think these are even used
         int dt;             // this is a copy of the input timestep
-        double dur;         // this is a copy of the input runTime, or the total amount of time to run the simulation for
+        double simDur;         // this is a copy of the input runTime, or the total amount of time to run the simulation for
         
 
         int numPar;        // this is a copy of the input numParticles to be released over the whole simulation
-        double srcX,srcY,srcZ;  // these are the source point x, y, and z values as copied from the input source information
+        // at some point in time, this needs to become the cumulative number of particles from each source, determined from each source
+        // instead of given as a single value that the sources all depend on
         
 
         // function for finding the largest sig value, which could be used for other similar datatypes if needed
