d72107901f60dd20237d9f2d0432090b7df28c2d
Behnam Bozorgmehr <behnambozorgmehr91@gmail.com>
2019-11-26 15:46:06 -0700

    Merging conflicts fixed

--- src/URBGeneralData.h
+++ src/URBGeneralData.h
@@ -147,48 +147,4 @@ public:
     std::vector<std::vector<int>> num_points;
     std::vector<std::vector<float>> coeff;
 
-<<<<<<< HEAD
-
-    /// Declaration of output manager
-    Output *output;
-
-    int output_counter=0;
-    float time=0;
-    std::vector<NcDim> dim_scalar_t;
-    std::vector<NcDim> dim_scalar_z;
-    std::vector<NcDim> dim_scalar_y;
-    std::vector<NcDim> dim_scalar_x;
-    std::vector<NcDim> dim_vector;
-    std::vector<NcDim> dim_vector_2d;
-    std::vector<std::string> output_fields;
-    struct AttScalarDbl {
-        float* data;
-        std::string name;
-        std::string long_name;
-        std::string units;
-        std::vector<NcDim> dimensions;
-    };
-    struct AttVectorDbl {
-        std::vector<float>* data;
-        std::string name;
-        std::string long_name;
-        std::string units;
-        std::vector<NcDim> dimensions;
-    };
-    struct AttVectorInt {
-        std::vector<int>* data;
-        std::string name;
-        std::string long_name;
-        std::string units;
-        std::vector<NcDim> dimensions;
-    };
-    std::map<std::string,AttScalarDbl> map_att_scalar_dbl;
-    std::map<std::string,AttVectorDbl> map_att_vector_dbl;
-    std::map<std::string,AttVectorInt> map_att_vector_int;
-    std::vector<AttScalarDbl> output_scalar_dbl;
-    std::vector<AttVectorDbl> output_vector_dbl;
-    std::vector<AttVectorInt> output_vector_int;
-
-=======
->>>>>>> caf166291ac4aedbe4660d280e28acad8137008c
 };
