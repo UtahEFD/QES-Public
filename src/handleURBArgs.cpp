769fd52373405fb8ab5f01bffde6056716eba1d3
fmargairaz <fab.margairaz@gmail.com>
2019-12-01 22:11:13 -0700

    compiling commandline fix

--- src/handleURBArgs.cpp
+++ src/handleURBArgs.cpp
@@ -1,8 +1,7 @@
 #include "handleURBArgs.h"
 
 URBArgs::URBArgs()
-	: verbose(false), quicFile(""), netCDFFileVz(""),netCDFFileWk("")
-		cellFace(false), solveType(1), compareType(0)
+  : verbose(false), quicFile(""), netCDFFileVz(""), netCDFFileWk(""), cellFace(false), solveType(1), compareType(0)
 {
     reg("help", "help/usage information", ArgumentParsing::NONE, '?');
     reg("verbose", "turn on verbose output", ArgumentParsing::NONE, 'v');
@@ -11,7 +10,7 @@ URBArgs::URBArgs()
     reg("juxtapositiontype", "selects a second solve method to compare to the original solve type", ArgumentParsing::INT, 'j');
     reg("quicproj", "Specifies the QUIC Proj file", ArgumentParsing::STRING, 'q');
     reg("netcdfvzout", "Specifies the netcdf file to write vizulatization results to", ArgumentParsing::STRING, 'o');
-		reg("netcdfwkout", "Specifies the netcdf file to write wroking file to", ArgumentParsing::STRING, 'w');
+    reg("netcdfwkout", "Specifies the netcdf file to write wroking file to", ArgumentParsing::STRING, 'w');
     reg("demfile", "Specifies the DEM file that should be used for terrain", ArgumentParsing::STRING, 'd');
     reg("icellout", "Specifies that the iCellFlag values should be output, this also will output cutCellFlags if they exist", ArgumentParsing::NONE, 'i');
     reg("terrainout", "Specifies that the triangle mesh for the terrain should be output", ArgumentParsing::NONE, 't');
@@ -36,7 +35,7 @@ void URBArgs::processArguments(int argc, char *argv[])
     if (quicFile != "") std::cout << "quicproj set to " << quicFile << std::endl;
 
     isSet( "netcdfvzout", netCDFFileVz );
-    if (netCDFFile != "") std::cout << "vizulatization netCDF output file set to " << netCDFFileVz << std::endl;
+    if (netCDFFileVz != "") std::cout << "vizulatization netCDF output file set to " << netCDFFileVz << std::endl;
 
 		isSet( "netcdfwkout", netCDFFileWk );
     if (netCDFFileWk != "") std::cout << "wroking file netCDF output file set to " << netCDFFileWk << std::endl;
