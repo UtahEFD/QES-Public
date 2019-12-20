f0951e5edebedee80fe31a4c08f9f54974ff8c20
Pete Willemsen <willemsn@d.umn.edu>
2019-12-19 19:52:00 -0600

    Cleaned up ray class - removed extraneous items.

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -5,11 +5,6 @@ Ray::Ray(float origin_x, float origin_y, float origin_z, Vector3<float> dirVec){
    this->origin_y = origin_y;
    this->origin_z = origin_z;
    this->dirVec = dirVec;
-
-   inverse_dir = Vector3<float>(1/dirVec[0], 1/dirVec[1], 1/dirVec[2]);
-   signs.push_back(inverse_dir[0] < 0);
-   signs.push_back(inverse_dir[1] < 0);
-   signs.push_back(inverse_dir[2] < 0);
 }
 
 Ray::Ray(float origin_x, float origin_y, float origin_z){
@@ -23,13 +18,6 @@ float Ray::getOriginY(){return origin_y;}
 float Ray::getOriginZ(){return origin_z;}
 Vector3<float> Ray::getDirection() {return dirVec;}
 
-Vector3<float> Ray::getInverseDir(){return inverse_dir;}
-std::vector<int> Ray::getSigns(){return signs;};
-
-void Ray::setDir(Vector3<float> dir){
+void Ray::setDir(const Vector3<float> &dir){
    dirVec = dir;
-   inverse_dir = Vector3<float>(1/dirVec[0], 1/dirVec[1], 1/dirVec[2]);
-   signs.push_back(inverse_dir[0] < 0);
-   signs.push_back(inverse_dir[1] < 0);
-   signs.push_back(inverse_dir[2] < 0);
 }
--- src/Ray.h
+++ src/Ray.h
@@ -12,20 +12,20 @@ class Ray{
   private:
       float origin_x, origin_y, origin_z;
       Vector3<float> dirVec;
-      Vector3<float> inverse_dir;  //inverse of dirVec
-      std::vector<int> signs;
+
   public:
       Ray(float origin_x, float origin_y, float origin_z, Vector3<float> dirVec);
 
       Ray(float origin_x, float origin_y, float origin_z);
 
+    ~Ray() {}
+
       float getOriginX();
       float getOriginY();
       float getOriginZ();
       Vector3<float> getDirection();
-      Vector3<float> getInverseDir();
-      std::vector<int> getSigns();
-      void setDir(Vector3<float> dir);
+
+      void setDir(const Vector3<float> &dir);
 };
 
 
