6f98a9814e8914db5e6ec6af48d3b57761ae6aec
Pete Willemsen <willemsn@d.umn.edu>
2020-04-10 14:47:53 -0500

    Added output to the GPU code to report the CUDA version, GPUs available and memory on the GPUs

--- src/DynamicParallelism.cu
+++ src/DynamicParallelism.cu
@@ -245,6 +245,81 @@ __global__ void SOR_iteration (float *d_lambda, float *d_lambda_old, int nx, int
 
 
 
+DynamicParallelism::DynamicParallelism(const URBInputData* UID, URBGeneralData* UGD)
+    : Solver(UID, UGD)
+{
+    std::cout << "DynamicParallelism Solver Initialization" << std::endl;
+  
+    int deviceCount = 0;
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+    if (error_id != cudaSuccess) {
+        std::cerr << "ERROR!   cudaGetDeviceCount returned "
+            << static_cast<int>(error_id) << "\n\t-> " 
+                  << cudaGetErrorString(error_id) << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // This function call returns 0 if there are no CUDA capable devices.
+    if (deviceCount == 0) {
+        std::cerr << "There are no available device(s) that support CUDA\n";
+        exit(EXIT_FAILURE);
+    } else {
+        std::cout << "\tDetected " << deviceCount << " CUDA Capable device(s)" << std::endl;
+    }
+
+    int dev, driverVersion = 0, runtimeVersion = 0;
+
+    for (dev = 0; dev < deviceCount; ++dev) {
+
+        cudaSetDevice(dev);
+
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+
+        std::cout << "\tDevice " << dev << ": " << deviceProp.name << std::endl;
+
+        // Console log
+        cudaDriverGetVersion(&driverVersion);
+        cudaRuntimeGetVersion(&runtimeVersion);
+        std::cout << "\t\tCUDA Driver Version / Runtime Version: " 
+                  << driverVersion / 1000 << "." << (driverVersion % 100) / 10 << " / " 
+                  << runtimeVersion / 1000 << "." <<  (runtimeVersion % 100) / 10 << std::endl;
+
+        std::cout << "\t\tCUDA Capability Major/Minor version number: "
+                  << deviceProp.major << "." << deviceProp.minor << std::endl;
+
+    char msg[256];
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+    sprintf_s(msg, sizeof(msg),
+              "  Total amount of global memory:                 %.0f MBytes "
+              "(%llu bytes)\n",
+              static_cast<float>(deviceProp.totalGlobalMem / 1048576.0f),
+              (unsigned long long)deviceProp.totalGlobalMem);
+#else
+    snprintf(msg, sizeof(msg),
+             "  Total amount of global memory:                 %.0f MBytes "
+             "(%llu bytes)\n",
+             static_cast<float>(deviceProp.totalGlobalMem / 1048576.0f),
+             (unsigned long long)deviceProp.totalGlobalMem);
+#endif
+    std::cout << msg;
+
+//    printf("  (%2d) Multiprocessors, (%3d) CUDA Cores/MP:     %d CUDA Cores\n",
+//           deviceProp.multiProcessorCount,
+//           _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor),
+//           _ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) *
+//           deviceProp.multiProcessorCount);
+
+    std::cout << "\t\tGPU Max Clock rate:  "
+              << deviceProp.clockRate * 1e-3f << " MHz ("
+              << deviceProp.clockRate * 1e-6f << " GHz)" << std::endl;
+    }
+    
+    
+}
+
+
 void DynamicParallelism::solve(const URBInputData* UID, URBGeneralData* UGD, bool solveWind)
 {
     auto startTotal = std::chrono::high_resolution_clock::now(); // Start
