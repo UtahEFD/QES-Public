8e5e0b767fad741c3496a74efb62344d7257f3d4
Behnam <behnambozorgmehr91@gmail.com>
2021-01-15 13:54:55 -0700

    Put terrain processing before sensor processing

--- src/WINDSGeneralData.cpp
+++ src/WINDSGeneralData.cpp
@@ -248,9 +248,38 @@ WINDSGeneralData::WINDSGeneralData(const WINDSInputData* WID, int solverType)
    }
 
    int halo_index_x = (WID->simParams->halo_x/dx);
-   WID->simParams->halo_x = halo_index_x*dx;
+   //WID->simParams->halo_x = halo_index_x*dx;
    int halo_index_y = (WID->simParams->halo_y/dy);
-   WID->simParams->halo_y = halo_index_y*dy;
+   //WID->simParams->halo_y = halo_index_y*dy;
+
+   if (WID->simParams->DTE_heightField)
+   {
+      // ////////////////////////////////
+      // Retrieve terrain height field //
+      // ////////////////////////////////
+      for (int i = 0; i < nx-1; i++)
+      {
+         for (int j = 0; j < ny-1; j++)
+         {
+            // Gets height of the terrain for each cell
+            int idx = i + j*(nx-1);
+            terrain[idx] = WID->simParams->DTE_mesh->getHeight(i * dx + dx * 0.5f, j * dy + dy * 0.5f);
+            if (terrain[idx] < 0.0)
+            {
+               terrain[idx] = 0.0;
+            }
+            id = i+j*nx;
+            for (size_t k=0; k<z.size()-1; k++)
+            {
+               terrain_id[id] = k;
+               if (terrain[idx] < z_face[k])
+               {
+                  break;
+               }
+            }
+         }
+       }
+     }
 
    //////////////////////////////////////////////////////////////////////////////////
    /////    Create sensor velocity profiles and generate initial velocity field /////
@@ -292,47 +321,42 @@ WINDSGeneralData::WINDSGeneralData(const WINDSInputData* WID, int solverType)
 
    if (WID->simParams->DTE_heightField)
    {
-      // ////////////////////////////////
-      // Retrieve terrain height field //
-      // ////////////////////////////////
-      auto start_stair = std::chrono::high_resolution_clock::now();
-      for (int i = 0; i < nx-1; i++)
+
+      if (WID->simParams->meshTypeFlag == 0 && WID->simParams->readCoefficientsFlag == 0)
       {
-         for (int j = 0; j < ny-1; j++)
-         {
-            // Gets height of the terrain for each cell
-            int idx = i + j*(nx-1);
-            terrain[idx] = WID->simParams->DTE_mesh->getHeight(i * dx + dx * 0.5f, j * dy + dy * 0.5f);
-            if (terrain[idx] < 0.0)
-            {
-               terrain[idx] = 0.0;
+        auto start_stair = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < nx-1; i++)
+        {
+           for (int j = 0; j < ny-1; j++)
+           {
+              // Gets height of the terrain for each cell
+              int idx = i + j*(nx-1);
+              id = i+j*nx;
+              for (size_t k=0; k<z.size()-1; k++)
+              {
+                 if (terrain[idx] < z[k+1])
+                 {
+                    break;
+                 }
+
+                 // ////////////////////////////////
+                 // Stair-step (original QUIC)    //
+                 // ////////////////////////////////
+                 int ii = i+WID->simParams->halo_x/dx;
+                 int jj = j+WID->simParams->halo_y/dy;
+                 int icell_cent = ii+jj*(nx-1)+(k+1)*(nx-1)*(ny-1);
+                 icellflag[icell_cent] = 2;
+
+
+                }
             }
-            id = i+j*nx;
-            for (size_t k=0; k<z.size()-1; k++)
-            {
-               terrain_id[id] = k+1;
-               if (terrain[idx] < z[k+1])
-               {
-                  break;
-               }
-               if (WID->simParams->meshTypeFlag == 0 && WID->simParams->readCoefficientsFlag == 0)
-               {
-                  // ////////////////////////////////
-                  // Stair-step (original QUIC)    //
-                  // ////////////////////////////////
-                  int ii = i+WID->simParams->halo_x/dx;
-                  int jj = j+WID->simParams->halo_y/dy;
-                  int icell_cent = ii+jj*(nx-1)+(k+1)*(nx-1)*(ny-1);
-                  icellflag[icell_cent] = 2;
-               }
-            }
-         }
+        }
+
+        auto finish_stair = std::chrono::high_resolution_clock::now();  // Finish recording execution time
+
+        std::chrono::duration<float> elapsed_stair = finish_stair - start_stair;
+        std::cout << "Elapsed time for terrain with stair-step: " << elapsed_stair.count() << " s\n";
       }
-
-      auto finish_stair = std::chrono::high_resolution_clock::now();  // Finish recording execution time
-
-      std::chrono::duration<float> elapsed_stair = finish_stair - start_stair;
-      std::cout << "Elapsed time for terrain with stair-step: " << elapsed_stair.count() << " s\n";
    }
    ///////////////////////////////////////////////////////
    //////   END of  Apply Terrain code       /////
@@ -502,177 +526,6 @@ WINDSGeneralData::WINDSGeneralData(const WINDSInputData* WID, int solverType)
    // data for the sort.
    std::cout << "Sorting buildings by height..." << std::endl;
    mergeSort( effective_height, allBuildingsV, building_id );
-
-   // ///////////////////////////////////////
-   //
-   // Mixing Length Ray Tracing Geometry Processing
-   //
-   // If we're calculating mixing length, make sure to add the
-   // buildings to the bvh used to calculate length scale
-   std::vector< Triangle* > buildingTris;
-
-   if (allBuildingsV.size() > 0) {
-
-      std::cout << "Preparing building geometry data for mixing length calculations." << std::endl;
-
-      // for (auto bIdx = 0u; bIdx <
-      // WID->buildings->buildings.size(); bIdx++)
-      for (auto bIdx = 0u; bIdx < allBuildingsV.size(); bIdx++)
-      {
-         // for each polyvert edge, create triangles of the sides
-         for (auto pIdx=0u; pIdx < allBuildingsV[bIdx]->polygonVertices.size(); pIdx++)
-         {
-            // each (x_poly, y_poly) represents 1 vertices of the
-            // polygon that is 2D.
-
-
-            // Form a line between (x_poly_i, y_poly_i) and (x_poly_i+1, y_poly_i+1)
-            //
-            // That line can be extruded to form a plane that could
-            // be decomposed into two triangles.
-            //
-            // Building has base_height -- should be the "ground"
-            // of the building.
-            //
-            // Building also has height_eff, which is the height of
-            // the building
-            //
-            // So triangle1 of face_i should be
-            // (x_poly_i, y_poly_i, base_height)
-            // (x_poly_i+1, y_poly_i+1, base_height)
-            // (x_poly_i+1, y_poly_i+1, base_height + height_eff)
-            //
-            // Then triangle2 of face_i should be
-            // (x_poly_i, y_poly_i, base_height)
-            // (x_poly_i+1, y_poly_i+1, base_height + height_eff)
-            // (x_poly_i, y_poly_i, base_height + height_eff)
-            //
-
-            if(pIdx == allBuildingsV[bIdx]->polygonVertices.size()-1){ //wrap around case for last vertices
-
-               //Triangle 1
-               Triangle *tri1 = new Triangle(Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx].y_poly,
-                                                            allBuildingsV[bIdx]->base_height),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[0].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[0].y_poly,
-                                                            allBuildingsV[bIdx]->base_height),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[0].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[0].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H)
-                                             );
-
-               //Triangle 2
-               Triangle *tri2 = new Triangle(Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[0].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[0].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[0].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[0].y_poly,
-                                                            allBuildingsV[bIdx]->base_height)
-                                             );
-
-               buildingTris.push_back(tri1);
-               buildingTris.push_back(tri2);
-
-            }else{
-
-               //Triangle 1
-               Triangle *tri1 = new Triangle(Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx].y_poly,
-                                                            allBuildingsV[bIdx]->base_height),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx+1].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx+1].y_poly,
-                                                            allBuildingsV[bIdx]->base_height),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx+1].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx+1].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H)
-                                             );
-
-               //Triangle 2
-               Triangle *tri2 = new Triangle(Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx+1].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx+1].y_poly,
-                                                            allBuildingsV[bIdx]->base_height+allBuildingsV[bIdx]->H),
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx+1].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx+1].y_poly,
-                                                            allBuildingsV[bIdx]->base_height)
-                                             );
-
-               buildingTris.push_back(tri1);
-               buildingTris.push_back(tri2);
-            }
-
-         } //end of for all building polygon vertices
-
-         // then create triangulated roof
-         // requires walking through the base_height + height_eff
-         // polygon plane and creating triangles...
-
-         // triangle fan starting at vertice 0 of the polygon
-
-         //Base Point (take the first polyvert edge and "fan" around
-         Vector3<float> baseRoofPt(allBuildingsV[bIdx]->polygonVertices[0].x_poly,
-                                   allBuildingsV[bIdx]->polygonVertices[0].y_poly,
-                                   allBuildingsV[bIdx]->height_eff);
-
-         for (auto pIdx=1u; pIdx < allBuildingsV[bIdx]->polygonVertices.size()-1; pIdx++){
-
-            Triangle* triRoof = new Triangle(baseRoofPt,
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx].y_poly,
-                                                            allBuildingsV[bIdx]->height_eff),
-
-                                             Vector3<float>(allBuildingsV[bIdx]->polygonVertices[pIdx+1].x_poly,
-                                                            allBuildingsV[bIdx]->polygonVertices[pIdx+1].y_poly,
-                                                            allBuildingsV[bIdx]->height_eff));
-
-            buildingTris.push_back(triRoof);
-
-         } //end of roof for loop
-      }
-   }
-
-
-   std::vector< Triangle* > groundTris(2);
-   if (WID->simParams->DTE_heightField == nullptr /*&& m_calcMixingLength*/) {
-
-      // need to make sure we add the ground plane triangles.  There
-      // // is no DEM in this case.
-      groundTris[0] = new Triangle( Vector3<float>(0.0, 0.0, 0.0), Vector3<float>(nx*dx, 0.0f, 0.0f), Vector3<float>(nx*dx, ny*dy, 0.0f) );
-      groundTris[1] = new Triangle( Vector3<float>(0.0, 0.0, 0.0), Vector3<float>(nx*dx, ny*dy, 0.0f), Vector3<float>(0.0f, ny*dy, 0.0f) );
-   }
-
-   // Assemble list of all triangles and create the mesh BVH
-   std::cout << "Forming Length Scale triangle mesh...\n";
-   std::vector<Triangle*> allTriangles;
-   if (WID->simParams->DTE_heightField) {
-
-      allTriangles.resize( WID->simParams->DTE_heightField->getTris().size() );
-
-      //std::copy(WID->simParams->DTE_heightField->getTris().begin(), WID->simParams->DTE_heightField->getTris().end(), allTriangles.begin());
-
-      for(int i = 0; i < WID->simParams->DTE_heightField->getTris().size(); i++){
-         allTriangles[i] = WID->simParams->DTE_heightField->getTris()[i];
-      }
-
-   }
-   else {
-      allTriangles.insert(allTriangles.end(), groundTris.begin(), groundTris.end());
-   }
-
-   // Add building triangles to terrain and/or ground triangle
-   for(int i = 0; i < buildingTris.size(); i++){
-       allTriangles.push_back(buildingTris[i]);
-   }
-
-   Mesh *m_mixingLengthMesh = new Mesh(allTriangles);
-   std::cout << "Triangle Meshing complete\n";
-   // ///////////////////////////////////////
 
    wall = new Wall();
 
