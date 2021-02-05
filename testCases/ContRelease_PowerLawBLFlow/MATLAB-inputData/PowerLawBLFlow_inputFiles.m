3bd5be25f0fa65021e3068b36e78e2a29af9c277
fmargairaz <fab.margairaz@gmail.com>
2021-02-05 16:17:42 -0700

    add test case power BL flow with continuous release

--- testCases/ContRelease_PowerLawBLFlow/MATLAB-inputData/PowerLawBLFlow_inputFiles.m
+++ testCases/ContRelease_PowerLawBLFlow/MATLAB-inputData/PowerLawBLFlow_inputFiles.m
@@ -33,10 +33,13 @@ y_cc=0.5*dy:dy:ly-0.5*dy;
 z_cc=-0.5*dz:dz:lz+0.5*dz; z_cc=z_cc';
 
 % set Power Law BL Flow param:
-a=4.8; % m^(1-p)/s
+a = 4.8; % m^(1-p)/s
+p = 0.15; 
+b = 0.08;
+n = 1;
+
 uRef = 5.90; % m/s
 hRef = 4.0; % m
-nPow = 0.15; 
 uStar = 0.20; % m/s
 C0 = 4.0;
 
@@ -46,7 +49,7 @@ C0 = 4.0;
 % face-center data:
 u_out = zeros(nx,ny,nz);
 
-uPowBL=a*(z_cc).^nPow;
+uPowBL=a*(z_cc).^p;
 % BC in the ghost cell:
 uPowBL(1)=-uPowBL(2); % ghost cell
 uPowBL(end)=uPowBL(end-1); % ghost cell
@@ -82,40 +85,63 @@ writeNetCDFFile_winds(filename,nx,ny,nz,x_cc,y_cc,z_cc,u,v,w,icellflag);
 %% ========================================================================
 % QES-TURB data:
 
-dudz = zeros(nz-1,1);
-for kk=2:nz-2
-    dudz(kk) = (uPowBL(kk+1)-uPowBL(kk-1))/(2*dz);
-end
-dudz(1) = -dudz(2);
+%dudz = zeros(nz-1,1);
+%for kk=2:nz-2
+%    dudz(kk) = (uPowBL(kk+1)-uPowBL(kk-1))/(2*dz);
+%end
+%dudz(1) = -dudz(2);
 
-k = 0.4*z_cc.*dudz/0.55;
+dudz=p*a*z_cc.^(p-1);
+
+ustar = 0.4*z_cc.*dudz;
+%ustar = 0.4*p*a*z_cc.^p;
+ustar(1) = -ustar(2);
+ustar(end) = ustar(end-1);
+
+uw = -b*z_cc.^n.*dudz;
+uw(1) = -uw(2);
+uw(end) = uw(end-1);
+
+%sigU = 2.30*ustar;
+%sigV = 1.80*ustar;
+%sigW = 1.00*ustar;
+
+sigU = 2.50*ustar;
+sigV = 1.78*ustar;
+sigW = 1.27*ustar;
+
+k = (ustar/0.55).^2;
 k(1) = -k(2);
 k(end) = k(end-1);
 
-nu = ((0.4*z_cc).^2).*(dudz);
+nu = 0.4.*z_cc.*ustar;
 nu(1) = -nu(2);
-nu(end) = nu(end-1);
 
-eps = 0.55*k.^(3/2)./(0.4*z_cc); 
+eps = 5.7*(sqrt(k)*0.55).^3./(0.4*z_cc);
 eps(1) = -eps(2);
 eps(end) = eps(end-1);
 
-
 % stress tensor
-tau11 = zeros(nx-1,ny-1,nz-1);
-tau22 = zeros(nx-1,ny-1,nz-1);
-tau33 = zeros(nx-1,ny-1,nz-1);
-tau13 = zeros(nx-1,ny-1,nz-1);
-tau12 = zeros(nx-1,ny-1,nz-1);
-tau23 = zeros(nx-1,ny-1,nz-1);
+txx = zeros(nx-1,ny-1,nz-1);
+txz = zeros(nx-1,ny-1,nz-1);
+txy = zeros(nx-1,ny-1,nz-1);
+tyy = zeros(nx-1,ny-1,nz-1);
+tyz = zeros(nx-1,ny-1,nz-1);
+tzz = zeros(nx-1,ny-1,nz-1);
 
-for kk=1:nz-1
-    tau11(:,:,kk) =  2.3438 * 2.0/3.0*k(kk);
-    tau22(:,:,kk) =  1.5000 * 2.0/3.0*k(kk);
-    tau33(:,:,kk) =  0.6338 * 2.0/3.0*k(kk);
-    
-    tau13(:,:,kk) = -nu(kk)*dudz(kk);
+for kk=2:nz-1
+    txx(:,:,kk) = sigU(kk)^2;%2.0/3.0*k(kk) * (2.0*0.55)^2;
+    tyy(:,:,kk) = sigV(kk)^2;%2.0/3.0*k(kk) * (2.0*0.55)^2;
+    tzz(:,:,kk) = sigW(kk)^2;%2.0/3.0*k(kk) * (1.3*0.55)^2;
 end
+txx(:,:,1) = -txx(:,:,2);
+tyy(:,:,1) = -tyy(:,:,2);
+tzz(:,:,1) = -tzz(:,:,2);
+
+for kk=2:nz-1
+    txz(:,:,kk) = -ustar(kk)^2;%nu(kk)*dudz(kk);
+end
+txz(:,:,1) = -txz(:,:,2);
 
 CoEps = zeros(nx-1,ny-1,nz-1);
 tke = zeros(nx-1,ny-1,nz-1);
@@ -123,14 +149,11 @@ tke = zeros(nx-1,ny-1,nz-1);
 for kk=1:nz-1
     tke(:,:,kk)=k(kk);
     %tke(:,:,k)=( z_cc(k)*C0*uStar^3/0.4/z_cc(k) )^(2/3);
-    CoEps(:,:,kk)=C0*eps(kk);
+    CoEps(:,:,kk)=eps(kk);
     %CoEps(:,:,k)=C0*uStar^3/0.4/z_cc(k);
 end
 
-
-
 % now save the netcdf turb output
-writeNetCDFFile_turb(filename,x_cc,y_cc,z_cc,CoEps,tke,tau11,tau12,tau13,tau22,tau23,tau33);
-
+writeNetCDFFile_turb(filename,x_cc,y_cc,z_cc,CoEps,tke,txx,txy,txz,tyy,tyz,tzz);
 
 
--- testCases/ContRelease_PowerLawBLFlow/MATLAB-results/mainPlumeResults.m
+++ testCases/ContRelease_PowerLawBLFlow/MATLAB-results/mainPlumeResults.m
@@ -8,23 +8,33 @@
 % Univesity of Utah. 2021
 
 H=4; % m
-U=5.90; % m/s 
-uStar=0.2; %m/s
+p=0.15;
+a=4.8;
+n=1;
+b=0.08;
+%b=0.059;
+
+U=a*H^p; % m/s 
+uStar=b/0.4; %m/s
+
+alpha=2+p-n;
+nu=(1-n)/alpha;
 
 % source info
-Q=100; % #par/s (source strength)
-tRelease=1000; % total time of release
+Q=200; % #par/s (source strength)
+tRelease=1400; % total time of release
 Ntot=Q*tRelease; % total number of particles
 
 % concentration info
 dt=1; % s
-tAvg=800; % s 
+tAvg=1200; % s 
 
 fsize=12;
 
 xS=20;yS=50;zS=4;
 
-xProf=[10.97,19.31]; % streamwise location 
+xProf=[6.0,11.0,19.0]; % streamwise location 
+%xProf=[5.42,10.97,19.31]; % streamwise location 
 
 % set the case base name for use in all the other file paths
 caseNameWinds = "PowerLawBLFlow";
@@ -57,40 +67,46 @@ boxDz=mean(diff(data.plume.z));
 boxVol=double(boxDx*boxDy*boxDz);
 CC=dt/tAvg/boxVol;
 
-
-p=0.15;
-a=4.8;
-n=1;
-b=0.059;
-
-alpha=2+p-n;
-nu=(1-n)/alpha;
-
 y=-50:0.1:50;
-z=0:0.01:5*H;
+z=0:0.1:5*H;
 [yy,zz]=ndgrid(y,z);
 
 for k=1:numel(xProf)
-    
+    %================================================
     idx1=find(round(100*xoH)>=round(100*xProf(k)),1);
-    idy1=find(data.plume.y>=yS,1);
-    idz1=find(data.plume.z>=zS,1);
-    
-    cStarPlume=squeeze(double(data.plume.pBox(idx1,:,:))*CC*(U*H*H/Q));
-    
     x=data.plume.x(idx1)-xS;
-    
-    idy2=find(y-y(1)>=data.plume.y(idy1),1);
-    idz2=find(z>=data.plume.z(idz1),1); 
-    
+    %================================================
+    cStarPlume=squeeze(double(data.plume.pBox(idx1,:,:))*CC*(U*H*H/Q));    
+    %================================================
     % from Seinfeld and Pandis 1998
     sigY=0.32*x^0.78;
-    
-    C=Q/((2*pi)^0.5*sigY)*exp(-0.5*yy.^2/sigY^2).*...
+    C=Q/(sqrt(2*pi)*sigY)*exp(-0.5*yy.^2/sigY^2).*...
         exp(-a*(zz.^alpha+H^alpha)/(b*alpha^2*x)).*(zz*H).^(0.5*(1-n))/(b*alpha*x).*...
         besseli(-nu,(2*a*(zz*H).^(0.5*alpha))/(b*alpha^2*x));
     cStarModel=C*(U*H*H/Q);
+    %================================================
+    idy1=find(data.plume.y>=yS,1);
+    idz1=find(data.plume.z>=zS,1);
+    %================================================
+    idy2=find(y-y(1)>=data.plume.y(idy1),1);
+    idz2=find(z>=data.plume.z(idz1),1); 
+    %================================================
     
+    %================================================
+    d2plotLat.xoH(k)=x/H;
+    d2plotLat.QPlume.yoH=yoH;
+    d2plotLat.QPlume.cStar(:,k)=cStarPlume(:,idz1);
+    d2plotLat.GModel.yoH=y/H+yS/H;
+    d2plotLat.GModel.cStar(:,k)=cStarModel(:,idz2);
+    %================================================
+    d2plotVert.xoH(k)=x/H;
+    d2plotVert.QPlume.zoH=zoH;
+    d2plotVert.QPlume.cStar(:,k)=cStarPlume(idy1,:);
+    d2plotVert.GModel.zoH=z/H;
+    d2plotVert.GModel.cStar(:,k)=cStarModel(idy2,:);
+    %================================================
+    
+    %================================================
     hfig = figure;
     set(hfig,'Units','centimeters')
     set(hfig,'defaulttextinterpreter','latex','DefaultAxesFontSize',fsize)
@@ -99,15 +115,15 @@ for k=1:numel(xProf)
     plot(yoH,cStarPlume(:,idz1),'s:','LineWidth',2)
     hold all
     plot((y+yS)/H,cStarModel(:,idz2),'-','LineWidth',2)
-    %xlim([.2 1.2])
     
     xlabel('$y/H$')
     ylabel('$C^*$')
+    grid on
     
     currentPlotName=sprintf('plotOutput/%s_%s_LatConc_%s',...
         caseNameWinds,caseNamePlume,strrep(sprintf('x%.2f',x/H),'.','o'));
     save2pdf(hfig,currentPlotName,hfig.Position(3:4),12)
-    
+    %================================================
     hfig = figure;
     set(hfig,'Units','centimeters')
     set(hfig,'defaulttextinterpreter','latex','DefaultAxesFontSize',fsize)
@@ -116,14 +132,14 @@ for k=1:numel(xProf)
     plot(cStarPlume(idy1,:),zoH,'s:','LineWidth',2)
     hold all
     plot(cStarModel(idy2,:),z/H,'-','LineWidth',2)
-
-    %ylim([.6 1.4])
     
     xlabel('$C^*$')
     ylabel('$z/H$')
+    grid on 
     
     currentPlotName=sprintf('plotOutput/%s_%s_VertConc_%s',...
            caseNameWinds,caseNamePlume,strrep(sprintf('x%.2f',x/H),'.','o'));
     save2pdf(hfig,currentPlotName,hfig.Position(3:4),12)
+    %================================================
 end
 
