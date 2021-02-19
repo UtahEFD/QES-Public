7e9ee64761885adb1700de638f44c9e2cd51bb10
fmargairaz <fab.margairaz@gmail.com>
2021-02-19 09:19:09 -0700

    changing time stamp to be optional

--- src/TimeSeries.h
+++ src/TimeSeries.h
@@ -18,12 +18,12 @@ public:
   std::vector<float> site_wind_dir, site_z_ref, site_U_ref;
   float site_one_overL;
   float site_canopy_H, site_atten_coeff;
-    std::string timeStamp;
+    std::string timeStamp="";
 
 
   virtual void parseValues()
   {
-      parsePrimitive<std::string>(true, timeStamp, "timeStamp");
+      parsePrimitive<std::string>(false, timeStamp, "timeStamp");
     parsePrimitive<int>(false, site_blayer_flag, "boundaryLayerFlag");
     parsePrimitive<float>(true, site_z0, "siteZ0");
     parsePrimitive<float>(true, site_one_overL, "reciprocal");
@@ -32,6 +32,12 @@ public:
     parseMultiPrimitives<float>(true, site_wind_dir, "direction");
     parsePrimitive<float>(false, site_canopy_H, "canopyHeight");
     parsePrimitive<float>(false, site_atten_coeff, "attenuationCoefficient");
+
+    if(timeStamp=="") {
+        std::cout << "[WARNING] no timestamp provided" << std::endl;
+       timeStamp="2020:01:01T00:00";
+    }
+        
   }
 
 };
