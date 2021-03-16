b325871b61583638a7b61fa8797a18390835c90d
moodymjm <moodymjm@gmail.com>
2021-03-16 16:13:41 -0600

    multiple ignition source

---
+++ src/ignition.h
@@ -0,0 +1,20 @@
+#include "util/ParseInterface.h"
+
+class ignition : public ParseInterface
+{
+private:
+
+public:
+	float xStart, yStart, length, width, baseHeight, height;
+
+	virtual void parseValues()
+	{
+
+    		parsePrimitive<float>(true, height,     "height");
+    		parsePrimitive<float>(true, baseHeight, "baseHeight");
+    		parsePrimitive<float>(true, xStart,     "xStart");
+    		parsePrimitive<float>(true, yStart,     "yStart");
+    		parsePrimitive<float>(true, length,     "length");
+    		parsePrimitive<float>(true, width,      "width");
+	}
+};
