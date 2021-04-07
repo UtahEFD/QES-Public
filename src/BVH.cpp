3964ae88d31927a9344a50b8a8f9c0b9ae84b71c
Jacob McCormick <jmccormick1858@yahoo.com>
2021-04-07 10:12:31 -0500

    moves inline functions to source file

--- src/BVH.cpp
+++ src/BVH.cpp
@@ -214,3 +214,107 @@ float BVH::heightToTri(float x, float y)
       return toL > toR ? toL : toR;
    }
 }
+
+bool BVH::rayBoxIntersect(const Ray &ray){
+   float originX = ray.getOriginX();
+   float originY = ray.getOriginY();
+   float originZ = ray.getOriginZ();
+   // Vector3<float> dir = ray.getDirection();
+   Vec3D dir = ray.getDirection();
+
+   float tMinX, tMaxX, tMinY, tMaxY, tMinZ, tMaxZ;
+
+   //calc tMinX and tMaxX
+   float aX = 1/dir[0];
+   if(aX >= 0){
+      tMinX = aX*(xmin - originX);
+      tMaxX= aX*(xmax - originX);
+   }else{
+      tMinX = aX*(xmax - originX);
+      tMaxX = aX*(xmin - originX);
+   }
+
+   //calc tMinY and tMaxY
+   float aY = 1/dir[1];
+   if(aY >= 0){
+      tMinY = aY*(ymin - originY);
+      tMaxY = aY*(ymax - originY);
+   }else{
+      tMinY = aY*(ymax - originY);
+      tMaxY = aY*(ymin - originY);
+   }
+
+   //calc tMinZ and tMaxZ
+   float aZ = 1/dir[2];
+   if(aZ >= 0){
+      tMinZ = aZ*(zmin - originZ);
+      tMaxZ = aZ*(zmax - originZ);
+   }else{
+      tMinZ = aZ*(zmax - originZ);
+      tMaxZ = aZ*(zmin - originZ);
+   }
+
+
+   if(tMinX > tMaxY || tMinY > tMaxX ||
+      tMinY > tMaxZ || tMinZ > tMaxY ||
+      tMinZ > tMaxX || tMinX > tMaxZ){
+      return false;
+   }else{
+      return true;
+   }
+}
+
+bool BVH::rayHit(const Ray &ray, HitRecord& rec)
+{
+   if(!rayBoxIntersect(ray)) {
+      return false;
+   }
+
+   if(isLeaf) {
+
+      //if(!tri){
+      //std::cout<<"Must not be a tri."<<std::endl;  // this is an error!
+      // }
+
+      float t0 = 0.00001;
+      //float t1 = std::numeric_limits<float>::infinity();
+      float t1 = 999999999.9;
+
+      return tri->rayTriangleIntersect(ray, rec, t0, t1);
+
+   } else {  //not a leaf node
+
+      bool leftHit = false, rightHit = false;
+      HitRecord lrec, rrec;
+
+      //prob don't need to check left due to BVH construction
+      //left should never be null for anything except a leaf node
+      if(leftBox){
+         leftHit = leftBox->rayHit(ray, lrec);
+      }
+
+      if(rightBox){
+         rightHit = rightBox->rayHit(ray, rrec);
+      }
+
+      // std::cout << "Left Hit: " << leftHit << ", Right Hit: " << rightHit << std::endl;
+
+      if(leftHit && rightHit){
+         if(lrec.hitDist < rrec.hitDist){
+            rec = lrec;
+         }else{
+            rec = rrec;
+         }
+         return true;
+      }else if(leftHit){
+         rec = lrec;
+         return true;
+      }else if(rightHit){
+         rec = rrec;
+         return true;
+      }else{
+         // std::cout << "false only else case" << std::endl;
+         return false;
+      }
+   }
+}
