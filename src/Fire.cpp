71b587a88664d2c017e0111c4250835a5722cac7
moodymjm <moodymjm@gmail.com>
2021-11-04 15:01:50 -0600

    40 fuel class fix

--- src/Fire.cpp
+++ src/Fire.cpp
@@ -154,7 +154,7 @@ Fire :: Fire(WINDSInputData* WID, WINDSGeneralData* WGD) {
 				//float fID = fuelMesh->getHeight(i*dx+dx*0.5f,j*dy+dy*0.5f);
 				float fID = fuelMesh->getHeight(i*dx,j*dy);				
 				//std::cout<<"fuelMesh ["<<i<<"]["<<j<<"] = "<<fID<<std::endl;
-				fuel_map[idx] = fID;
+				fuel_map[idx] = round(fID);
 				int fuelID = round(fID);
 					
 				if (fuelID==1)  	fire_cells[idx].fuel = new ShortGrass();
@@ -175,7 +175,47 @@ Fire :: Fire(WINDSInputData* WID, WINDSGeneralData* WGD) {
 				else if (fuelID==93) fire_cells[idx].fuel = new Agricultural();
 				else if (fuelID==98) fire_cells[idx].fuel = new Water();
 				else if (fuelID==99) fire_cells[idx].fuel = new Bare();
-				else fire_cells[idx].fuel = new Bare();
+				else if (fuelID==101) fire_cells[idx].fuel = new GR1();
+				else if (fuelID==102) fire_cells[idx].fuel = new GR2();
+				else if (fuelID==103) fire_cells[idx].fuel = new GR3();
+				else if (fuelID==104) fire_cells[idx].fuel = new GR4();
+				else if (fuelID==105) fire_cells[idx].fuel = new GR5();
+				else if (fuelID==106) fire_cells[idx].fuel = new GR6();
+				else if (fuelID==107) fire_cells[idx].fuel = new GR7();
+				else if (fuelID==108) fire_cells[idx].fuel = new GR8();
+				else if (fuelID==109) fire_cells[idx].fuel = new GR9();
+				else if (fuelID==121) fire_cells[idx].fuel = new GS1();
+				else if (fuelID==122) fire_cells[idx].fuel = new GS2();
+				else if (fuelID==123) fire_cells[idx].fuel = new GS3();
+				else if (fuelID==124) fire_cells[idx].fuel = new GS4();
+				else if (fuelID==141) fire_cells[idx].fuel = new SH1();
+				else if (fuelID==142) fire_cells[idx].fuel = new SH2();
+				else if (fuelID==143) fire_cells[idx].fuel = new SH3();
+				else if (fuelID==144) fire_cells[idx].fuel = new SH4();
+				else if (fuelID==145) fire_cells[idx].fuel = new SH5();
+				else if (fuelID==146) fire_cells[idx].fuel = new SH6();
+				else if (fuelID==147) fire_cells[idx].fuel = new SH7();
+				else if (fuelID==148) fire_cells[idx].fuel = new SH8();
+				else if (fuelID==149) fire_cells[idx].fuel = new SH9();
+				else if (fuelID==161) fire_cells[idx].fuel = new TU1();
+				else if (fuelID==162) fire_cells[idx].fuel = new TU2();
+				else if (fuelID==163) fire_cells[idx].fuel = new TU3();
+				else if (fuelID==164) fire_cells[idx].fuel = new TU4();
+				else if (fuelID==165) fire_cells[idx].fuel = new TU5();
+				else if (fuelID==181) fire_cells[idx].fuel = new TL1();
+				else if (fuelID==182) fire_cells[idx].fuel = new TL2();
+				else if (fuelID==183) fire_cells[idx].fuel = new TL3();
+				else if (fuelID==184) fire_cells[idx].fuel = new TL4();
+				else if (fuelID==185) fire_cells[idx].fuel = new TL5();
+				else if (fuelID==186) fire_cells[idx].fuel = new TL6();
+				else if (fuelID==187) fire_cells[idx].fuel = new TL7();
+				else if (fuelID==188) fire_cells[idx].fuel = new TL8();
+				else if (fuelID==189) fire_cells[idx].fuel = new TL9();
+				else if (fuelID==201) fire_cells[idx].fuel = new SB1();
+				else if (fuelID==202) fire_cells[idx].fuel = new SB2();
+				else if (fuelID==203) fire_cells[idx].fuel = new SB3();
+				else if (fuelID==204) fire_cells[idx].fuel = new SB4();
+				else fire_cells[idx].fuel = new SH1();
 				
 			}
 		}
@@ -202,7 +242,47 @@ Fire :: Fire(WINDSInputData* WID, WINDSGeneralData* WGD) {
 				if (fuel_type==92) fire_cells[idx].fuel = new Snow();
 				if (fuel_type==93) fire_cells[idx].fuel = new Agricultural();
 				if (fuel_type==98) fire_cells[idx].fuel = new Water();
-				if (fuel_type==99) fire_cells[idx].fuel = new Bare();				
+				if (fuel_type==99) fire_cells[idx].fuel = new Bare();
+				if (fuel_type==101) fire_cells[idx].fuel = new GR1();
+				if (fuel_type==102) fire_cells[idx].fuel = new GR2();
+				if (fuel_type==103) fire_cells[idx].fuel = new GR3();
+				if (fuel_type==104) fire_cells[idx].fuel = new GR4();
+				if (fuel_type==105) fire_cells[idx].fuel = new GR5();
+				if (fuel_type==106) fire_cells[idx].fuel = new GR6();
+				if (fuel_type==107) fire_cells[idx].fuel = new GR7();
+				if (fuel_type==108) fire_cells[idx].fuel = new GR8();
+				if (fuel_type==109) fire_cells[idx].fuel = new GR9();
+				if (fuel_type==121) fire_cells[idx].fuel = new GS1();
+				if (fuel_type==122) fire_cells[idx].fuel = new GS2();
+				if (fuel_type==123) fire_cells[idx].fuel = new GS3();
+				if (fuel_type==124) fire_cells[idx].fuel = new GS4();
+				if (fuel_type==141) fire_cells[idx].fuel = new SH1();
+				if (fuel_type==142) fire_cells[idx].fuel = new SH2();
+				if (fuel_type==143) fire_cells[idx].fuel = new SH3();
+				if (fuel_type==144) fire_cells[idx].fuel = new SH4();
+				if (fuel_type==145) fire_cells[idx].fuel = new SH5();
+				if (fuel_type==146) fire_cells[idx].fuel = new SH6();
+				if (fuel_type==147) fire_cells[idx].fuel = new SH7();
+				if (fuel_type==148) fire_cells[idx].fuel = new SH8();
+				if (fuel_type==149) fire_cells[idx].fuel = new SH9();
+				if (fuel_type==161) fire_cells[idx].fuel = new TU1();
+				if (fuel_type==162) fire_cells[idx].fuel = new TU2();
+				if (fuel_type==163) fire_cells[idx].fuel = new TU3();
+				if (fuel_type==164) fire_cells[idx].fuel = new TU4();
+				if (fuel_type==165) fire_cells[idx].fuel = new TU5();
+				if (fuel_type==181) fire_cells[idx].fuel = new TL1();
+				if (fuel_type==182) fire_cells[idx].fuel = new TL2();
+				if (fuel_type==183) fire_cells[idx].fuel = new TL3();
+				if (fuel_type==184) fire_cells[idx].fuel = new TL4();
+				if (fuel_type==185) fire_cells[idx].fuel = new TL5();
+				if (fuel_type==186) fire_cells[idx].fuel = new TL6();
+				if (fuel_type==187) fire_cells[idx].fuel = new TL7();
+				if (fuel_type==188) fire_cells[idx].fuel = new TL8();
+				if (fuel_type==189) fire_cells[idx].fuel = new TL9();
+				if (fuel_type==201) fire_cells[idx].fuel = new SB1();
+				if (fuel_type==202) fire_cells[idx].fuel = new SB2();
+				if (fuel_type==203) fire_cells[idx].fuel = new SB3();
+				if (fuel_type==204) fire_cells[idx].fuel = new SB4();		
 			}
 		}
 	std::cout<<"fuel set constant = "<<fuel_type<<std::endl;		
@@ -950,7 +1030,7 @@ struct Fire::FireProperties Fire :: balbi(FuelProperties* fuel,float u_mid, floa
 
       // fuel constants
       float m        = fmc_g;                   ///< fuel particle moisture content [0-1]
-      float sigma    = oneHour;					///< dead fine fuel load [kg/m^2]
+      float sigma    = oneHour*0.2471;		///< dead fine fuel load [kg/m^2]
       float sigmaT   = fgi;                    	///< total fine fuel load [kg/m^2]
       //float rhoFuel  = 1500;                    ///< fuel density [kg/m^3]
       float T_i      = 600;                     ///< ignition temp [k]
@@ -1004,8 +1084,8 @@ struct Fire::FireProperties Fire :: balbi(FuelProperties* fuel,float u_mid, floa
       float A = fmin(SAV/(2*pi),beta/betaT)*Chi_0*cmbcnst/(4*q);     ///< Radiant coefficient [eq.13]
     
       // Initial guess = Rothermel ROS 
-      //float R = rothermel(fuel,max(u_mid,v_mid),alpha,fmc_g);       ///< Total Rate of Spread (ROS) [m/s]
-		float R = 0.1;
+      float R = rothermel(fuel,max(u_mid,v_mid),alpha,fmc_g);       ///< Total Rate of Spread (ROS) [m/s]
+	//float R = 0.1;
       // Initial tilt angle guess = slope angle
       float gamma  = alpha;    ///< Flame tilt angle
       float maxIter = 100;
@@ -1054,8 +1134,19 @@ struct Fire::FireProperties Fire :: balbi(FuelProperties* fuel,float u_mid, floa
         
         // Total ROS 
         R    = ROSBase + ROSFlame + ROSConv;
+	if(R < 0){
+		std::cout<<"ROS Base = "<<ROSBase<<": ROS Flame = "<<ROSFlame<<": ROS Conv = "<<ROSConv<<": gamma = "<<gamma<<": psi = "<<psi<<std::endl;
+		R = 0;
+	}
         error = std::abs(R-R_old);
-        R_old = R;
+        
+	iter++;
+	if(iter == 99){
+		std::cout<<"Balbi iteration reached: R = "<<R<<": R old = "<<R_old<<": one hour =  "<<oneHour<<std::endl;
+		std::cout<<"u = "<<u_mid<<": v = "<<v_mid<<": xnorm = "<<x_norm<<": ynorm = "<<y_norm<<": xslope = "<<x_slope<<": yslope = "<<y_slope<<std::endl;
+	}
+
+	R_old = R;
       }
         
       // calculate flame depth
