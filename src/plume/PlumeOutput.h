1f8563884518802ead159f60cf0dff6162a1a824
fmargairaz <fab.margairaz@gmail.com>
2022-06-25 11:47:35 -0600

    switching plumeOutput to QEStime system and new averaging time system

--- src/plume/PlumeOutput.h
+++ src/plume/PlumeOutput.h
@@ -62,8 +62,7 @@ public:
   // setup and save output for the given time
   // in this case the saved data is output averaged concentration
   // This is the one function that needs called from outside after constructor time
-  void save(float currentTime);
-  void save(QEStime) {}
+  void save(QEStime);
 
 private:
   // default constructor
@@ -71,27 +70,23 @@ private:
 
   // time averaging frequency control information
   // in this case, this is also the output control information
-  float timeAvgStart;// time to start concentration averaging, not the time to start output. Adjusted if the time averaging duration does not divide evenly by the averaging frequency
-  float timeAvgEnd;// time to end concentration averaging and output
-  float timeAvgFreq;// time averaging frequency and output frequency
-
+  // time to start concentration averaging, not the time to start output.
+  QEStime averagingStartTime;
+  // averaging period in seconds
+  float averagingPeriod;
+  //
+  float ongoingAveragingTime;
 
   // variables needed for getting proper averaging and output time control
-  float nextOutputTime;// next output time value that is updated each time save is called and output occurs. Also initializes a restart of the particle binning for the next time averaging period
+  // next output time value that is updated each time save is called and output occurs.
+  // Also initializes a restart of the particle binning for the next time averaging period
+  QEStime nextOutputTime;
 
   // pointer to the class that save needs to use to get the data for the concentration calculation
-  Plume *plume;
-
-
-  // need nx, ny, nz of the domain to make sure the output handles domains that are not three dimensional
-  // for now these are a copy of the input QES-Winds values
-  int nx;
-  int ny;
-  int nz;
+  Plume *m_plume;
 
   // need the simulation timeStep for use in concentration averaging
   float timeStep;
-
 
   // Sampling box variables for calculating concentration data
   // Number of boxes to use for the sampling box
@@ -105,7 +100,6 @@ private:
   std::vector<float> xBoxCen, yBoxCen, zBoxCen;// list of x,y, and z points for the concentration sampling box information
   std::vector<int> pBox;// sampling box particle counter (for average)
   std::vector<float> conc;// concentration values (for output)
-
 
   // function for counting the number of particles in the sampling boxes
   void boxCount();
