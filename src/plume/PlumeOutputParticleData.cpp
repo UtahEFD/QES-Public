806fd27528eb5749ae621fbdb8518e9225ca3fe8
Lucas Ulmer <lucas.d.ulmer@gmail.com>
2022-09-29 10:49:51 -0600

    taking plume files from fm-xml-rework, except files i've modified

--- src/plume/PlumeOutputParticleData.cpp
+++ src/plume/PlumeOutputParticleData.cpp
@@ -1,14 +1,15 @@
 /****************************************************************************
- * Copyright (c) 2021 University of Utah
- * Copyright (c) 2021 University of Minnesota Duluth
+ * Copyright (c) 2022 University of Utah
+ * Copyright (c) 2022 University of Minnesota Duluth
  *
- * Copyright (c) 2021 Behnam Bozorgmehr
- * Copyright (c) 2021 Jeremy A. Gibbs
- * Copyright (c) 2021 Fabien Margairaz
- * Copyright (c) 2021 Eric R. Pardyjak
- * Copyright (c) 2021 Zachary Patterson
- * Copyright (c) 2021 Rob Stoll
- * Copyright (c) 2021 Pete Willemsen
+ * Copyright (c) 2022 Behnam Bozorgmehr
+ * Copyright (c) 2022 Jeremy A. Gibbs
+ * Copyright (c) 2022 Fabien Margairaz
+ * Copyright (c) 2022 Eric R. Pardyjak
+ * Copyright (c) 2022 Zachary Patterson
+ * Copyright (c) 2022 Rob Stoll
+ * Copyright (c) 2022 Lucas Ulmer
+ * Copyright (c) 2022 Pete Willemsen
  *
  * This file is part of QES-Plume
  *
@@ -303,7 +304,6 @@ void PlumeOutputParticleData::save(QEStime timeIn)
 
       uFluct[parID] = (*parItr)->uFluct;
       vFluct[parID] = (*parItr)->vFluct;
-
       wFluct[parID] = (*parItr)->wFluct;
 
       delta_uFluct[parID] = (*parItr)->delta_uFluct;
