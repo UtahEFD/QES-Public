f264b66bc6eff4561bdc1d1eefc823e5de2e0ac8
fmargairaz <fab.margairaz@gmail.com>
2023-02-13 11:31:32 -0700

    fix and check calcTimer utility class

--- src/util/calcTime.h
+++ src/util/calcTime.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <vector>
-
+#include <map>
 #include <chrono>
 
 
@@ -27,13 +27,25 @@ private:
   // utility functions
   bool isTimerStored(const std::string &timerName);
   size_t findTimerIdx(const std::string &timerName);
-  void calcTimeDistribution(const double &totalSeconds, int &milliseconds, int &seconds, int &minutes, int &hours, int &days);
-  void printTime(const std::string &timerName, const int &milliseconds, const int &seconds, const int &minutes, const int &hours, const int &days);
+  void calcTimeDistribution(const double &totalSeconds,
+                            int &milliseconds,
+                            int &seconds,
+                            int &minutes,
+                            int &hours,
+                            int &days);
+  void printTime(const std::string &timerName,
+                 const int &milliseconds,
+                 const int &seconds,
+                 const int &minutes,
+                 const int &hours,
+                 const int &days);
 
   // data members
   std::vector<std::string> storedTimerNames;
-  // std::vector<std::chrono::time_point<std::chrono::system_clock,std::chrono::duration<long,std::ratio<1,1000000000> > > > storedStartTimes;
+  //std::vector<std::chrono::time_point<std::chrono::system_clock,std::chrono::duration<long,std::ratio<1,1000000000> > > > storedStartTimes;
   std::vector<std::chrono::time_point<std::chrono::high_resolution_clock>> storedStartTimes;
+
+  std::map<std::string, std::chrono::time_point<std::chrono::high_resolution_clock>> storedTimers;
 };
 
 #endif// CALCTIME_H
