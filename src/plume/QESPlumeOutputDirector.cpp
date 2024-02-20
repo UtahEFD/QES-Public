7b37dd3e1997b7363e24bf622159249f2f2560f6
Fabien Margairaz <fab.margairaz@gmail.com>
2024-02-20 15:20:13 -0700

    prototype for output director

---
+++ src/plume/QESPlumeOutputDirector.cpp
@@ -0,0 +1,44 @@
+/****************************************************************************
+ * Copyright (c) 2022 University of Utah
+ * Copyright (c) 2022 University of Minnesota Duluth
+ *
+ * Copyright (c) 2022 Behnam Bozorgmehr
+ * Copyright (c) 2022 Jeremy A. Gibbs
+ * Copyright (c) 2022 Fabien Margairaz
+ * Copyright (c) 2022 Eric R. Pardyjak
+ * Copyright (c) 2022 Zachary Patterson
+ * Copyright (c) 2022 Rob Stoll
+ * Copyright (c) 2022 Lucas Ulmer
+ * Copyright (c) 2022 Pete Willemsen
+ *
+ * This file is part of QES-Plume
+ *
+ * GPL-3.0 License
+ *
+ * QES-Plume is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * QES-Plume is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QES-Plume. If not, see <https://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+/** @file QESPlumeOutputDirector.cpp
+ * @brief
+ */
+
+#include "QESPlumeOutputDirector.h"
+
+void PLUMEOutputDirector::enroll(QESOutputInterface *, const std::string &, FileType)
+{
+}
+
+void PLUMEOutputDirector::save(const QEStime &timeIn)
+{
+  for (auto f : files) { f->save(timeIn); }
+}
