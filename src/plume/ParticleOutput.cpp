57e0729dcb905e7bed486e073b122bde10448a41
Fabien Margairaz <fab.margairaz@gmail.com>
2024-04-29 17:06:43 -0600

    adding back particle data output using visitor and csv for easier framework

--- src/plume/ParticleOutput.cpp
+++ src/plume/ParticleOutput.cpp
@@ -30,4 +30,130 @@
 
 /** @file ParticleOutput.cpp
  */
+
 #include "ParticleOutput.h"
+#include "PLUMEGeneralData.h"
+
+ParticleOutput::ParticleOutput(PI_ParticleOutputParameters *params, PLUMEGeneralData *PGD)
+{
+  // output frequency
+  outputFrequency = params->outputFrequency;
+  // time to start output
+  if (params->outputStartTime < 0) {
+    outputStartTime = PGD->getSimTimeStart();
+    // set the initial next output time value
+    nextOutputTime = outputStartTime + outputFrequency;
+  } else {
+    outputStartTime = PGD->getSimTimeStart() + params->outputStartTime;
+    // set the initial next output time value
+    nextOutputTime = outputStartTime;
+  }
+}
+
+void ParticleOutput::save(QEStime &t, PLUMEGeneralData *PGD)
+{
+  if (t >= nextOutputTime) {
+    for (const auto &pm : PGD->models) {
+      pm.second->accept(new ExportParticleData(t, PGD));
+    }
+    nextOutputTime = nextOutputTime + outputFrequency;
+  }
+}
+
+ExportParticleData::ExportParticleData(QEStime &t, PLUMEGeneralData *PGD)
+  : time(t)
+{
+  fname_prefix = PGD->plumeParameters.outputFileBasename;
+  fname_suffix = "particleData_";
+  fname_suffix.append(time.getTimestamp());
+  fname_suffix.append(".csv");
+
+  file_prologue.append("# Simulation start time: " + PGD->getSimTimeStart().getTimestamp() + "\n");
+  file_prologue.append("# Simulation current time: " + time.getTimestamp() + "\n");
+}
+
+void ExportParticleData::visit(TracerParticle_Model *pm)
+{
+  std::string sout = fname_prefix + "_" + pm->tag + "_" + fname_suffix;
+
+  // opens an existing csv file or creates a new file.
+  fstream fout;
+  fout.open(sout, ios::out);
+  fout << "# Particle Data File\n";
+  fout << "# Model particle tag: " << pm->tag << "\n";
+  fout << file_prologue;
+  fout << "#particleID,tStrt,sourceIdx,d,m,wdecay,"
+       << "xPos_init,yPos_init,"
+          "zPos_init,xPos,yPos,zPos,"
+          "uFluct,vFluct,wFluct,"
+          "delta_uFluct,delta_vFluct,delta_wFluct\n";
+
+  for (auto k = 0u; k < pm->get_particles()->size(); ++k) {// Insert the data to file
+    TracerParticle *p = pm->get_particles()->get(k);
+    if (p->isActive && !p->isRogue) {
+      fout << p->particleID << ", "
+           << p->tStrt << ", "
+           << p->sourceIdx << ", "
+           << p->d << ", "
+           << p->m << ", "
+           << p->wdecay << ", "
+           << p->xPos_init << ", "
+           << p->yPos_init << ", "
+           << p->zPos_init << ", "
+           << p->xPos << ", "
+           << p->yPos << ", "
+           << p->zPos << ", "
+           << p->uFluct << ", "
+           << p->vFluct << ", "
+           << p->wFluct << ", "
+           << p->delta_uFluct << ", "
+           << p->delta_vFluct << ", "
+           << p->delta_wFluct << "\n";
+    }
+  }
+
+  fout.close();
+}
+
+void ExportParticleData::visit(HeavyParticle_Model *pm)
+{
+  std::string sout = fname_prefix + "_" + pm->tag + "_" + fname_suffix;
+
+  // opens an existing csv file or creates a new file.
+  fstream fout;
+  fout.open(sout, ios::out);
+  fout << "# Particle Data File\n";
+  fout << "# Model particle tag: " << pm->tag << "\n";
+  fout << file_prologue;
+  fout << "#particleID,tStrt,sourceIdx,d,m,wdecay,"
+       << "xPos_init,yPos_init,"
+          "zPos_init,xPos,yPos,zPos,"
+          "uFluct,vFluct,wFluct,"
+          "delta_uFluct,delta_vFluct,delta_wFluct\n";
+
+  for (auto k = 0u; k < pm->get_particles()->size(); ++k) {// Insert the data to file
+    HeavyParticle *p = pm->get_particles()->get(k);
+    if (p->isActive && !p->isRogue) {
+      fout << p->particleID << ", "
+           << p->tStrt << ", "
+           << p->sourceIdx << ", "
+           << p->d << ", "
+           << p->m << ", "
+           << p->wdecay << ", "
+           << p->xPos_init << ", "
+           << p->yPos_init << ", "
+           << p->zPos_init << ", "
+           << p->xPos << ", "
+           << p->yPos << ", "
+           << p->zPos << ", "
+           << p->uFluct << ", "
+           << p->vFluct << ", "
+           << p->wFluct << ", "
+           << p->delta_uFluct << ", "
+           << p->delta_vFluct << ", "
+           << p->delta_wFluct << "\n";
+    }
+  }
+
+  fout.close();
+}
