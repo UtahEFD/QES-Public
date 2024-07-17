b74d6348e71cf7af24052e61410f0ec8c7b4d4ba
fmargairaz <fab.margairaz@gmail.com>
2024-07-17 15:31:30 -0600

    some comments un unit test

--- tests/unitTests/plume_IDGenerator.cpp
+++ tests/unitTests/plume_IDGenerator.cpp
@@ -5,11 +5,13 @@
 
 TEST_CASE("Testing ID generator class")
 {
+  // 1st ptr to the generator
   IDGenerator *id_gen;
   id_gen = IDGenerator::getInstance();
 
   uint32_t test;
 
+  // test the generator
   test = id_gen->get();
   REQUIRE(test == 0);
   test = id_gen->get();
@@ -17,16 +19,17 @@ TEST_CASE("Testing ID generator class")
   test = id_gen->get();
   REQUIRE(test == 2);
 
-
+  // 2nd ptr to the generator
   IDGenerator *id_gen_2;
   id_gen_2 = IDGenerator::getInstance();
 
+  // test that the same id generator provide the same ids
   test = id_gen_2->get();
   REQUIRE(test == 3);
 
+  // test the generator for vector
   std::vector<uint32_t> test_vec;
   test_vec.resize(10, 0);
-
   id_gen_2->get(test_vec);
   REQUIRE(test_vec[0] == 4);
   REQUIRE(test_vec[9] == 13);
