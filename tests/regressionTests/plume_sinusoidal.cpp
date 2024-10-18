46483d60258cb0c1db538af7a507461220167624
Fabien Margairaz <fab.margairaz@gmail.com>
2024-10-18 15:13:48 -0600

    cleaning regression tests

--- tests/regressionTests/plume_sinusoidal.cpp
+++ tests/regressionTests/plume_sinusoidal.cpp
@@ -1,6 +1,39 @@
-//
-// Created by Fabien Margairaz on 3/24/23.
-//
+/****************************************************************************
+ * Copyright (c) 2024 University of Utah
+ * Copyright (c) 2024 University of Minnesota Duluth
+ *
+ * Copyright (c) 2024 Behnam Bozorgmehr
+ * Copyright (c) 2024 Jeremy A. Gibbs
+ * Copyright (c) 2024 Fabien Margairaz
+ * Copyright (c) 2024 Eric R. Pardyjak
+ * Copyright (c) 2024 Zachary Patterson
+ * Copyright (c) 2024 Rob Stoll
+ * Copyright (c) 2024 Lucas Ulmer
+ * Copyright (c) 2024 Pete Willemsen
+ *
+ * This file is part of QES-Winds
+ *
+ * GPL-3.0 License
+ *
+ * QES-Winds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * QES-Winds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QES-Winds. If not, see <https://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+/**
+ * @file plume_sinusoidal.cpp
+ * @brief This is a regression test for the well-mixed condition for an arbitrary sinusoidal
+ * stress tensor
+ */
+
 #include <catch2/catch_test_macros.hpp>
 
 #include <iostream>
@@ -8,14 +41,16 @@
 #include <algorithm>
 
 #include "util/calcTime.h"
-
-#include "qes/Domain.h"
-
-#include "plume/PlumeInputData.hpp"
 #include "util/NetCDFInput.h"
 #include "util/QESout.h"
 
+#include "qes/Domain.h"
+
+#include "winds/WINDSGeneralData.h"
+
+#include "plume/PlumeInputData.hpp"
 #include "plume/PLUMEGeneralData.h"
+#include "plume/TracerParticle_Concentration.h"
 
 float calcEntropy(int nbrBins, ManagedContainer<TracerParticle> *particles);
 void calcRMSE_wFluct(int nbrBins, ManagedContainer<TracerParticle> *particles, std::map<std::string, float> &rmse);
--- tests/regressionTests/plume_uniform.cpp
+++ tests/regressionTests/plume_uniform.cpp
@@ -1,6 +1,38 @@
-//
-// Created by Fabien Margairaz on 4/2/23.
-//
+/****************************************************************************
+ * Copyright (c) 2024 University of Utah
+ * Copyright (c) 2024 University of Minnesota Duluth
+ *
+ * Copyright (c) 2024 Behnam Bozorgmehr
+ * Copyright (c) 2024 Jeremy A. Gibbs
+ * Copyright (c) 2024 Fabien Margairaz
+ * Copyright (c) 2024 Eric R. Pardyjak
+ * Copyright (c) 2024 Zachary Patterson
+ * Copyright (c) 2024 Rob Stoll
+ * Copyright (c) 2024 Lucas Ulmer
+ * Copyright (c) 2024 Pete Willemsen
+ *
+ * This file is part of QES-Winds
+ *
+ * GPL-3.0 License
+ *
+ * QES-Winds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * QES-Winds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QES-Winds. If not, see <https://www.gnu.org/licenses/>.
+ ****************************************************************************/
+
+/**
+ * @file plume_uniform.cpp
+ * @brief This is a regression test based on the uniform Gaussian plume model
+ */
+
 #include <catch2/catch_test_macros.hpp>
 
 #include <iostream>
@@ -8,14 +40,15 @@
 #include <algorithm>
 
 #include "util/calcTime.h"
-
-#include "plume/PlumeInputData.hpp"
 #include "util/NetCDFInput.h"
 #include "util/QESout.h"
 
-#include "winds/WINDSGeneralData.h"
-#include "plume/PLUMEGeneralData.h"
+#include "qes/Domain.h"
 
+#include "winds/WINDSGeneralData.h"
+
+#include "plume/PlumeInputData.hpp"
+#include "plume/PLUMEGeneralData.h"
 #include "plume/TracerParticle_Concentration.h"
 
 // float calcEntropy(int nbrBins, Plume *plume);
