4acb71d45c818483532ea248296fa1f84536139f
Fabien Margairaz <fab.margairaz@gmail.com>
2024-12-02 20:22:28 -0700

    adding the option to obtain a reference to avoid copy

--- tests/unitTests/QESDataTransport.h
+++ tests/unitTests/QESDataTransport.h
@@ -5,7 +5,7 @@
 
 /**
  * \brief QES Data Transport dictionary container.
- * 
+ *
  * This class can be used to contain data in a name, value pair
  * system. The intended use would be to transport data between
  * components in way that is decoupled from the rest of the implementation.
@@ -31,14 +31,14 @@ public:
   template<typename T>
   void put(const std::string &key, T value)
   {
-    dataStorageContainer[key] = value;
+    dataStorageContainer[key] = value;// should this be a move?
   }
 
   /**
    * /brief Obtains the value associated with a key and returns it.  The template
    * type is required when called. If the type is incorrect, it will throw
    * a bad cast exception.
-   * 
+   *
    * Retrieves the value associate with the key. The value will be cast to the
    * type specified in the template function call. If the cast is unsuccessful,
    * a runtime_error exception will be thrown to indicate the type was not correct.
@@ -65,6 +65,36 @@ public:
   }
 
   /**
+   * /brief Obtains the reference to the value associated with a key and returns it.
+   * The template type is required when called. If the type is incorrect,
+   * it will throw a bad cast exception.
+   *
+   * Retrieves the value associate with the key. The value will be cast to the
+   * type specified in the template function call. If the cast is unsuccessful,
+   * a runtime_error exception will be thrown to indicate the type was not correct.
+   *
+   * If the key is not in the container, a runtime_error exception will be thrown to
+   * indicate the key was not found.
+   *
+   * @param key   The key of the data to retrieve.
+   * @return The reference to the value associated with the key, cast to the specified type.
+   * @throws std::bad_any_cast if the value is not of the expected type.
+   */
+  template<typename T>
+  T &get_ref(const std::string &key)
+  {
+    if (dataStorageContainer.find(key) == dataStorageContainer.end()) {
+      throw std::runtime_error("Key not found: " + key);
+    }
+
+    try {
+      return std::any_cast<T &>(dataStorageContainer.at(key));
+    } catch (const std::bad_any_cast &e) {
+      throw std::runtime_error("Type mismatch for key: " + key);
+    }
+  }
+
+  /**
    * /brief Function to help see if the container contains a key.
    *
    * This function will query the container to determine if it contains the
@@ -80,5 +110,4 @@ public:
   {
     return dataStorageContainer.find(key) != dataStorageContainer.end();
   }
-
 };
